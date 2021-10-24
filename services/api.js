5ea243da045bfb2a92ae7c783ce4f2b6c186ec4b
LAURO DE SA <itrafor@gmail.com>
2021-10-24 18:24:29 -0300

    axios 2

--- services/api.js
+++ services/api.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({    
     baseURL: 'https://itsimplesbk.vercel.app/',
-    headers: {'Access-Control-Allow-Origin': '*'}
+    headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+              }
 
 });
 
