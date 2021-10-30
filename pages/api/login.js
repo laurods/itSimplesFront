ec619d947f3fd94240f0f5c28b26b4854a709a74
LAURO DE SA <itrafor@gmail.com>
2021-10-30 11:42:37 -0300

    conn 6

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,17 +1,15 @@
-import peopleDAO from '../../models/login';
+import connection from '../../config/conn';
 const HTTP200 = 200;
 const HTTP500 = 500;
 export default async (req, res) => {
-    try {
-        const { email, password } = req.body; 
-        console.log(email);
-        console.log(password);     
-        //const authPeople = await peopleDAO.findOne(email, password);
-        //return res.status(HTTP200).json(authPeople);
-        return res.status(HTTP200).json({ message: 'login efetuado' });
-        
-      } catch (err) {
-        return res.status(HTTP500).json({ message: err.message });
-      } 
+    const { email, password } = req.body; 
+    const people = await connection().then((db) =>
+		db
+			.collection("people")
+			.findOne({ email })
+	);
+        if(!people) return ({ message: 'Email incorreto!' }); 
+	
+	return (people); 
 
 }
