7381cdd902ad5617e5fca8808feeceb710c7f0a3
LAURO DE SA <itrafor@gmail.com>
2021-11-01 19:29:52 -0300

    config/conn 4

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,20 +1,7 @@
-import connectToDatabase from '../../config/conn';
-const HTTP200 = 200;
-const HTTP500 = 500;
+//import connectToDatabase from '../../config/conn';
+
 export default async (req, res) => {
     const { email, password } = req.body; 
-	console.log(email);
-	const { db, client } = await connectToDatabase();
-	if(client.isConnected()) {
-		const people = await db
-		.collection("people")
-		.findOne({ email })
-
-	  if(people) {
-		return res.status(200).json({ people })
-	  }
-	}
-
-	return res.status(500).json({ error: 'client DB is not connected' })
+	console.log(email);	
 
 }
