091a3a4bf0fcad7347c0479b088a56d0464fc271
LAURO DE SA <itrafor@gmail.com>
2021-11-01 20:25:52 -0300

    login 10

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,11 @@
 import connectToDatabase from '../../config/conn';
 export default async (req, res) => {
 	const email = req.body.email;
-	const { db, client } = await connectToDatabase(); 
-	return res.status(200).json({db: db, client:client})	
+	const { db, client } = await connectToDatabase();
+	if(client.isConnected()) {
+		return res.status(200).json({msg: 'conectou'})
+	}
+
+	return res.status(200).json({msg:'erro'})	
 
 }
