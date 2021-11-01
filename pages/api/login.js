26c87d61c2028c5cd7e255b56ede7389c3104804
LAURO DE SA <itrafor@gmail.com>
2021-11-01 20:00:46 -0300

    login 5

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,8 +1,6 @@
 //import connectToDatabase from '../../config/conn';
-
 export default async (req, res) => {
-    const { email, password } = req.body; 
-	console.log(email);
-	return res.status(200).json({ email })	
+
+	return res.status(200).json({msg: 'teste'})	
 
 }
