53d9663858b7e9e767de66f42c112b657020ab44
LAURO DE SA <itrafor@gmail.com>
2021-11-02 06:33:54 -0300

    subscribers 2

--- pages/login.js
+++ pages/login.js
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { AuthContext } from '../contexts/AuthContext';
+import axios from 'axios';
 
 const theme = createTheme();
 
@@ -26,7 +27,8 @@ export default function Login() {
       password: data.get('password'),
     });
     console.log(values);
-    signIn(values);
+    axios.post('/api/login', { email: data.get('email')})
+    //signIn(values);
   };
 
   return (
