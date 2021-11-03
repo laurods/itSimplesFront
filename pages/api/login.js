0837b93f76b95260fe897fa9c923723d08dd6263
LAURO DE SA <itrafor@gmail.com>
2021-11-03 19:59:31 -0300

    login 031101

--- pages/api/login.js
+++ pages/api/login.js
@@ -9,29 +9,17 @@ const client = new MongoClient(url);
                       
  module.exports = async (req, res) => {
     try {
-         await client.connect();
-         console.log("Connected correctly to server");
+        const { email, password } = req.body; 
+         await client.connect();         
          const db = client.db(dbName);
 
          // Use the collection "people"
-         const col = db.collection("people");
-
-         // Construct a document                                                                                                                                                              
-         let personDocument = {
-             "name": { "first": "Alan", "last": "Turing" },
-             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
-             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
-             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
-             "views": 1250000
-         }
-
-         // Insert a single document, wait for promise so we can read it back
-         const p = await col.insertOne(personDocument);
+         const col = db.collection("people");         
          // Find one document
-         const myDoc = await col.findOne();
-         // Print to the console
-         console.log(myDoc);
-         res.status(200).json(personDocument);
+         const myDoc = await col.findOne({ email });
+         if(!myDoc) return res.status(500).json({ message: 'Email incorreto!' });
+
+         res.status(200).json({ myDoc });
 
         } catch (err) {
          console.log(err.stack);
@@ -41,4 +29,3 @@ const client = new MongoClient(url);
         await client.close();
     }
 }
-
