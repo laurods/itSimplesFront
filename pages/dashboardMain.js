13cb6eda99df634101834c6bf2ee9207e2a7d823
LAURO DE SA <itrafor@gmail.com>
2021-11-05 07:14:11 -0300

    update 051106

--- pages/dashboardMain.js
+++ pages/dashboardMain.js
@@ -19,7 +19,7 @@ export default function Dashboard() {
       }
       loadAll();
     }, []);
-    // setCNPJsByUsers([])
+    setCNPJsByUsers(clients)
     // setActiveCNPJ('354254')
     // setMovimentosCNPJ([])
  
