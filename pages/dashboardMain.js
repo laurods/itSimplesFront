445b0639db81e5a32cbc76a3f2d3efe6714cc252
LAURO DE SA <itrafor@gmail.com>
2021-11-05 07:05:21 -0300

    update 051104

--- pages/dashboardMain.js
+++ pages/dashboardMain.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import nookies from 'nookies';
 import { AuthContext } from '../contexts/AuthContext';
@@ -7,13 +7,18 @@ import Content from '../components/dashboard/content.js';
 
 export default function Dashboard() {
      const { userId, isAuthenticated, setCNPJsByUsers, setActiveCNPJ, setMovimentosCNPJ } = useContext(AuthContext);
-  
-    const loadAll = async() =>{
-      const clients = await axios.post('/api/cnpjbyuser', { user: userId });
-      const list = clients.data;
-      console.log(list); 
-    } 
+     const [clients, setClients] = useState([]);
+     console.log(`clientes ${clients}`);
     
+    useEffect(() => {
+      const loadAll = async() =>{
+        const clients = await axios.post('/api/cnpjbyuser', { user: userId });
+        const list = clients.data;
+        console.log(`lista ${list}`); 
+        setClients(list)        
+      }
+      loadAll();
+    }, []);
     // setCNPJsByUsers([])
     // setActiveCNPJ('354254')
     // setMovimentosCNPJ([])
