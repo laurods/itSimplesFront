f604a537688c2e39cab1fa41ec7ca7f49ec21cfb
LAURO DE SA <itrafor@gmail.com>
2021-11-06 13:03:56 -0300

    update 061107

--- pages/dashboard/[cnpj].js
+++ pages/dashboard/[cnpj].js
@@ -1,24 +1,27 @@
 import React, { useContext, useEffect } from 'react';
 import { useRouter } from 'next/router'
-import api from '../../services/api';
-import nookies from 'nookies';
+import axios from 'axios';
 import Top from '../../components/dashboard/top.js';
 import Content from '../../components/dashboard/content.js';
 
 export default function Dashboard() {
+  const { userId, setCNPJsByUsers, setActiveCNPJ, setMovimentosCNPJ } = useContext(AuthContext);
   const router = useRouter()
   const { cnpj } = router.query;
    console.log(cnpj);
-    // useEffect(() => {
-    //   const loadAll = async() =>{        
-    //     const movimentosByCNPJ = await axios.post('/api/movimentosbycnpj', { cnpj: listClients[0].cnpj });
-    //     const listMovimentos = movimentosByCNPJ.data;
-    //     setCNPJsByUsers(listClients)
-    //     setActiveCNPJ(listClients[0])
-    //     setMovimentosCNPJ(listMovimentos)        
-    //   }
-    //   loadAll();
-    // }, []);
+   console.log(userId);
+   useEffect(() => {
+    const loadAll = async() =>{
+      const clients = await axios.post('/api/cnpjbyuser', { user: userId });
+      const listClients = clients.data;
+      const movimentosByCNPJ = await axios.post('/api/movimentosbycnpj', { cnpj: cnpj });
+      const listMovimentos = movimentosByCNPJ.data;
+      setCNPJsByUsers(listClients)
+      setActiveCNPJ(listClients[0])
+      setMovimentosCNPJ(listMovimentos)        
+    }
+    loadAll();
+  }, []);
 
     return (
       <>
