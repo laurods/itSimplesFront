81e3bb053b0673ba33f79c62c2a073b0462a8552
LAURO DE SA <itrafor@gmail.com>
2021-11-06 12:14:48 -0300

    update 061106

--- pages/dashboard/[cnpj].js
+++ pages/dashboard/[cnpj].js
@@ -1,16 +1,25 @@
 import React, { useContext, useEffect } from 'react';
+import { useRouter } from 'next/router'
 import api from '../../services/api';
 import nookies from 'nookies';
-import { AuthContext } from '../../contexts/AuthContext';
 import Top from '../../components/dashboard/top.js';
 import Content from '../../components/dashboard/content.js';
 
-export default function Dashboard({empresas, activeCNPJ, movimentos}) {
-    const { userId, isAuthenticated, setCNPJsByUsers, setActiveCNPJ, setMovimentosCNPJ } = useContext(AuthContext);
-    console.log(empresas);
-    setCNPJsByUsers(empresas)
-    setActiveCNPJ(activeCNPJ)
-    setMovimentosCNPJ(movimentos)
+export default function Dashboard() {
+  const router = useRouter()
+  const { cnpj } = router.query;
+   console.log(cnpj);
+    // useEffect(() => {
+    //   const loadAll = async() =>{        
+    //     const movimentosByCNPJ = await axios.post('/api/movimentosbycnpj', { cnpj: listClients[0].cnpj });
+    //     const listMovimentos = movimentosByCNPJ.data;
+    //     setCNPJsByUsers(listClients)
+    //     setActiveCNPJ(listClients[0])
+    //     setMovimentosCNPJ(listMovimentos)        
+    //   }
+    //   loadAll();
+    // }, []);
+
     return (
       <>
       <Top />
@@ -18,36 +27,4 @@ export default function Dashboard({empresas, activeCNPJ, movimentos}) {
       </>
     );
     
-  }
-
-export async function getServerSideProps(ctx){
-    const cookies = nookies.get(ctx)
-  
-    const objUser = {
-      user: cookies.idUser
-    }
-    
-    const cnpjByUser = await api.post('cnpjbyuser', objUser).then(res => {  
-      return (res.data);            
-    });
-
-    const objCNPJ = {
-      cnpj: ctx.query.cnpj
-    }
-
-    const movimentosByCNPJ = await api.post('dashboard/movimentosbycnpj', objCNPJ).then(res => {  
-      return (res.data);            
-    });
-
-    
-   
-    
-    
-    return{
-      props: {
-        empresas: cnpjByUser,
-        activeCNPJ:ctx.query.cnpj,        
-        movimentos: movimentosByCNPJ
-      }
-    }
   }
--- pages/dashboardMain.js
+++ pages/dashboardMain.js
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
-import nookies from 'nookies';
 import { AuthContext } from '../contexts/AuthContext';
 import Top from '../components/dashboard/top.js';
 import Content from '../components/dashboard/content.js';
@@ -14,19 +13,13 @@ export default function Dashboard() {
         const listClients = clients.data;
         const movimentosByCNPJ = await axios.post('/api/movimentosbycnpj', { cnpj: listClients[0].cnpj });
         const listMovimentos = movimentosByCNPJ.data;
-        console.log(listClients[0].cnpj);
-        console.log(listClients);        
-        console.log(listMovimentos);
         setCNPJsByUsers(listClients)
         setActiveCNPJ(listClients[0])
         setMovimentosCNPJ(listMovimentos)        
       }
       loadAll();
     }, []);
-    // setCNPJsByUsers(clients)
-    // setActiveCNPJ('354254')
-    // setMovimentosCNPJ([])
- 
+
     return (
       <>
       <Top />
