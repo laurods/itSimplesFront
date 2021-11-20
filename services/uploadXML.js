d018b0b369af57e4728ca7375713f25b3196e7db
LAURO DE SA <itrafor@gmail.com>
2021-11-20 17:00:32 -0300

    upload xml 21

--- services/uploadXML.js
+++ services/uploadXML.js
@@ -1,6 +1,8 @@
 //import api from './api';
 import axios from 'axios';
 const parseString = require('xml2js').parseString;
+const fs = require('fs');
+
 const handleUpload = (files) => {
     const uploadedFiles = files.map((file) => ({
       file,
@@ -12,9 +14,11 @@ const handleUpload = (files) => {
   };
 /* Inicio processXML*/  
 const processXML = (uploadedFile) => {
-  const xml = uploadedFile;
+  //const xml = uploadedFile;
+  const xml = fs.readFileSync(uploadedFile);
   console.log('aki 2')
   console.log(xml)
+
   parseString(xml, {trim: true}, function (err, result) {
      console.dir(result);
    });
