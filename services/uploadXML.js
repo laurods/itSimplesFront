17528cb5d3d670f51075776f00dfd776de148224
LAURO DE SA <itrafor@gmail.com>
2021-11-20 17:31:42 -0300

    upload xml 25

--- services/uploadXML.js
+++ services/uploadXML.js
@@ -15,7 +15,7 @@ const handleUpload = (files) => {
 /* Inicio processXML*/  
 const processXML = (uploadedFile) => {
   const xml = uploadedFile;
-  //const xml = fs.readFileSync(uploadedFile);
+  const xml = fs.readFileSync(uploadedFile);
   console.log('aki 2')
   console.log(xml)
 
