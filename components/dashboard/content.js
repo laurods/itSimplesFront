df542f8e63f67e3d0e2fc315951b8b03a7ed770d
LAURO DE SA <itrafor@gmail.com>
2021-11-20 15:04:24 -0300

    upload xml 9

--- components/dashboard/content.js
+++ components/dashboard/content.js
@@ -19,16 +19,19 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 async function uploadServiceXML(files) {
+  const config = {
+    headers: { 'content-type': 'multipart/form-data' },    
+  };
   console.log(files[0]);
   
   const fileData = new FormData();
   fileData.append('file', files[0]);  
-  axios.post('/api/xmlcompras', fileData).then(res => {
+  axios.post('/api/xmlcompras', fileData, config).then(res => {
     console.log(res);
-    //console.log(res.data)                      
+    console.log(res.data)                      
   })
   .catch((error) => {
-    //console.log(error.res.data);
+    console.log(error.res.data);
     console.log(error.res.status);
     console.log(error.res.headers);
   });
--- pages/api/xmlcompras.js
+++ pages/api/xmlcompras.js
@@ -1,11 +1,9 @@
-                      
- module.exports = async (req, res) => {
-    try {
-        const xml = req.file.buffer;   
-          res.status(200).json(xml);       
-
-        } catch (err) {
-         console.log(err.stack);
-     } 
-    
-}
+export default (req, res) => {
+  if (req.method === 'POST') {
+    // Process a POST request
+    res.status(200).json({ data: 'success' });
+  } else {
+    // Handle any other HTTP method
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  }
+};
