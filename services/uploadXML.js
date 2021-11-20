da22851e56fe4fbb47bf78a260ba1bf6c1249d48
LAURO DE SA <itrafor@gmail.com>
2021-11-20 16:18:54 -0300

    upload xml 17

--- services/uploadXML.js
+++ services/uploadXML.js
@@ -1,6 +1,6 @@
 //import api from './api';
 import axios from 'axios';
-
+const parseString = require('xml2js').parseString;
 const handleUpload = (files) => {
     const uploadedFiles = files.map((file) => ({
       file,
@@ -12,8 +12,13 @@ const handleUpload = (files) => {
   };
 /* Inicio processXML*/  
 const processXML = (uploadedFile) => {
- console.log(uploadedFile);
- 
+  const xml = uploadedFile;
+  parseString(xml, {trim: true}, function (err, result) {
+    console.dir(result);
+  });
+
+
+
 };
 
 /* Fim processXML*/
