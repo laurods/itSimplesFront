be67885ee7679a6f84a4559a55828b2c842e2a97
LAURO DE SA <itrafor@gmail.com>
2021-11-20 15:22:22 -0300

    upload xml 12

--- pages/api/xmlcompras.js
+++ pages/api/xmlcompras.js
@@ -1,8 +1,8 @@
 export default (req, res) => {
   if (req.method === 'POST') {
     // Process a POST request
-    const xml = req.file.buffer;
-    res.status(200).json({ data: xml });
+    const xml = req.file
+    res.status(200).json({ data: 'success', xml: xml });
   } else {
     // Handle any other HTTP method
     res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
