6d6a24f5d08d1874c0c9b2c98907a5b7bb00b322
LAURO DE SA <itrafor@gmail.com>
2021-11-20 15:48:12 -0300

    upload xml 14

--- pages/api/xmlcompras.js
+++ pages/api/xmlcompras.js
@@ -1,20 +1,21 @@
 const parseString = require('xml2js').parseString;
-export default (req, res) => {
+
+export default (req, res) => { 
   if (req.method === 'POST') {
     // Process a POST request
     const xml = req.file.buffer;
 
-    const parseFromXML = (xml) => {
-      return new Promise((resolve, reject) => {
-        parseString(xml, { mergeAttrs: true, explicitArray: false}, function (err, ok) {
-          if (err) return resolve(err);
-          return resolve(ok);
-        });
-      });
-    };
+    // const parseFromXML = (xml) => {
+    //   return new Promise((resolve, reject) => {
+    //     parseString(xml, { mergeAttrs: true, explicitArray: false}, function (err, ok) {
+    //       if (err) return resolve(err);
+    //       return resolve(ok);
+    //     });
+    //   });
+    // };
 
-    const result = await parseFromXML(xml);
-    res.status(200).json({ data: result });
+    //const result = await parseFromXML(xml);
+    res.status(200).json({ data: 'success' });
   } else {
     // Handle any other HTTP method
     res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
