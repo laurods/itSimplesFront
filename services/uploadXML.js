d7179946dca8d2213a6fe96385546596c8e11608
LAURO DE SA <itrafor@gmail.com>
2021-11-23 06:09:20 -0300

    xml 17

--- services/uploadXML.js
+++ services/uploadXML.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const parseString = require('xml2js').parseString;
 const handleUpload = (files) => {
     const uploadedFiles = files.map((file) => ({
       file,
@@ -11,15 +12,30 @@ const handleUpload = (files) => {
   };
 
 /* Inicio processXML*/  
-const processXML = (uploadedFile) => {   
+const processXML = async (uploadedFile) => {   
   console.log('aki 2')
-  const dataFile = new FormData();
-  dataFile.append('file', uploadedFile.file);
-  axios.post('/api/xmlcompras', dataFile).then(res => {
-    console.log(res.status);
-    console.log(res.data);                         
-  });
+  // const dataFile = new FormData();
+  // dataFile.append('file', uploadedFile.preview);
+  // axios.post('/api/xmlcompras', dataFile).then(res => {
+  //   console.log(res.status);
+  //   console.log(res.data);                         
+  // });
+  const text = "<bookstore><book>" +
+"<title>Everyday Italian</title>" +
+"<author>Giada De Laurentiis</author>" +
+"<year>2005</year>" +
+"</book></bookstore>";
 
+const parseFromXML = (xml) => {
+  return new Promise((resolve, reject) => {
+    parseString(xml, { mergeAttrs: true, explicitArray: false}, function (err, ok) {
+      if (err) return resolve(err);
+      return resolve(ok);
+    });
+  });
+};
+const dataNF = await parseFromXML(text);
+console.log(dataNF);
 };
 
 /* Fim processXML*/
