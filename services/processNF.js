49131d01d129f09709f6008fdc58d48216293528
LAURO DE SA <itrafor@gmail.com>
2021-11-23 20:47:14 -0300

    xml 34

--- services/processNF.js
+++ services/processNF.js
@@ -59,75 +59,75 @@ const allIcms = products.map((item) => { // Padronizando os dados dos valores de
     return item.imposto.ICMS.ICMS10;
 });
 
-//  const allIPI = products.map((item) => {
-//     if(!item.imposto.IPI.IPITrib){ // Padronizando os dados dos valores de IPI
-//         const tributos = {
-//             CST: '0',
-//             vBC: '0',
-//             pIPI: '0',
-//             vIPI: '0',
-//         }
-//         return tributos;
-//       }
-//       return item.imposto.IPI.IPITrib;
-//  });
+ const allIPI = products.map((item) => {
+    if(!item.imposto.IPI.IPITrib){ // Padronizando os dados dos valores de IPI
+        const tributos = {
+            CST: '0',
+            vBC: '0',
+            pIPI: '0',
+            vIPI: '0',
+        }
+        return tributos;
+      }
+      return item.imposto.IPI.IPITrib;
+ });
 
-//  const allPIS = products.map((item) => {
-//     if(!item.imposto.PIS.PISAliq){ // Padronizando os dados dos valores de PIS
-//         const tributos = {
-//             CST: '0',
-//             vBC: '0',
-//             pPIS: '0',
-//             vPIS: '0',
-//         }
-//         return tributos;
-//       }
-//       return item.imposto.PIS.PISAliq;
-//  });
+ const allPIS = products.map((item) => {
+    if(!item.imposto.PIS.PISAliq){ // Padronizando os dados dos valores de PIS
+        const tributos = {
+            CST: '0',
+            vBC: '0',
+            pPIS: '0',
+            vPIS: '0',
+        }
+        return tributos;
+      }
+      return item.imposto.PIS.PISAliq;
+ });
 
-//  const allCOFINS = products.map((item) => {
-//     if(!item.imposto.COFINS.COFINSAliq){ // Padronizando os dados dos valores de COFINS
-//         const tributos = {
-//             CST: '0',
-//             vBC: '0',
-//             pCOFINS: '0',
-//             vCOFINS: '0',
-//         }
-//         return tributos;
-//       }
-//       return item.imposto.COFINS.COFINSAliq; 
-//  });
+ const allCOFINS = products.map((item) => {
+    if(!item.imposto.COFINS.COFINSAliq){ // Padronizando os dados dos valores de COFINS
+        const tributos = {
+            CST: '0',
+            vBC: '0',
+            pCOFINS: '0',
+            vCOFINS: '0',
+        }
+        return tributos;
+      }
+      return item.imposto.COFINS.COFINSAliq; 
+ });
 
-//   const calculateTax = products.map((item, index) => {
-//         return {
-//         nf,
-//         cnpjEmitente,    
-//         cnpjDestinatario,
-//         sitTrib: sitTrib[index].code,
-//         cfop: allProducts[index].cfop,
-//         cst: allIcms[index].CST,
-//         ncm: allProducts[index].ncm,
-//         cest: allProducts[index].cest,
-//         cean: allProducts[index].cean, //Codigo de barras da caixa
-//         name: allProducts[index].name,
-//         quant: parseFloat(allProducts[index].quant).toFixed(2),
-//         total: parseFloat(allProducts[index].total).toFixed(2),
-//         icms: allIcms[index].vICMS,
-//         ipi: allIPI[index].vIPI,
-//         pis: allPIS[index].vPIS,
-//         cofins: allCOFINS[index].vCOFINS,
-//         pDifICMS: parseInt(pIcmsRS - (allIcms[index].pICMS)), // percentual diferença ICMS (RS - 18% BASE)
-//         vDifICMS: ((parseInt(pIcmsRS - (allIcms[index].pICMS))/100) * parseFloat(allProducts[index].total)).toFixed(2),  // valor diferença ICMS
-//         vICMSST: allIcms[index].vICMSST, //valor icms sustituição tributária
-//         //..............valor total dos produtos............+...valor do IPI ..................+..........................diferençao de icms interestadual ..............................+............ sustituição tributária
-//         custoTotal: ((parseFloat(allProducts[index].total)) + (parseFloat(allIPI[index].vIPI)) + (parseInt(pIcmsRS - (allIcms[index].pICMS))/100) * parseFloat(allProducts[index].total) + (parseFloat(allIcms[index].vICMSST))).toFixed(2),
-//         custoUnitario: (((parseFloat(allProducts[index].total)) + (parseFloat(allIPI[index].vIPI)) + (parseInt(pIcmsRS - (allIcms[index].pICMS))/100) * parseFloat(allProducts[index].total) + (parseFloat(allIcms[index].vICMSST))) / parseFloat(allProducts[index].quant)).toFixed(2),
-//       }
+  const calculateTax = products.map((item, index) => {
+        return {
+        nf,
+        cnpjEmitente,    
+        cnpjDestinatario,
+        sitTrib: sitTrib[index].code,
+        cfop: allProducts[index].cfop,
+        cst: allIcms[index].CST,
+        ncm: allProducts[index].ncm,
+        cest: allProducts[index].cest,
+        cean: allProducts[index].cean, //Codigo de barras da caixa
+        name: allProducts[index].name,
+        quant: parseFloat(allProducts[index].quant).toFixed(2),
+        total: parseFloat(allProducts[index].total).toFixed(2),
+        icms: allIcms[index].vICMS,
+        ipi: allIPI[index].vIPI,
+        pis: allPIS[index].vPIS,
+        cofins: allCOFINS[index].vCOFINS,
+        pDifICMS: parseInt(pIcmsRS - (allIcms[index].pICMS)), // percentual diferença ICMS (RS - 18% BASE)
+        vDifICMS: ((parseInt(pIcmsRS - (allIcms[index].pICMS))/100) * parseFloat(allProducts[index].total)).toFixed(2),  // valor diferença ICMS
+        vICMSST: allIcms[index].vICMSST, //valor icms sustituição tributária
+        //..............valor total dos produtos............+...valor do IPI ..................+..........................diferençao de icms interestadual ..............................+............ sustituição tributária
+        custoTotal: ((parseFloat(allProducts[index].total)) + (parseFloat(allIPI[index].vIPI)) + (parseInt(pIcmsRS - (allIcms[index].pICMS))/100) * parseFloat(allProducts[index].total) + (parseFloat(allIcms[index].vICMSST))).toFixed(2),
+        custoUnitario: (((parseFloat(allProducts[index].total)) + (parseFloat(allIPI[index].vIPI)) + (parseInt(pIcmsRS - (allIcms[index].pICMS))/100) * parseFloat(allProducts[index].total) + (parseFloat(allIcms[index].vICMSST))) / parseFloat(allProducts[index].quant)).toFixed(2),
+      }
 
-//   })             
+  })             
   
-//   //return calculateTax;
-//   console.log(calculateTax)
+  //return calculateTax;
+  console.log(calculateTax)
 }
 
 module.exports = {
