f089395db068bfbf70f83f0ffb57858987c6de89
LAURO DE SA <itrafor@gmail.com>
2021-11-23 05:03:54 -0300

    xml 6

--- services/uploadXML.js
+++ services/uploadXML.js
@@ -1,8 +1,4 @@
-//import api from './api';
 import axios from 'axios';
-const parseString = require('xml2js').parseString;
-
-
 const handleUpload = (files) => {
     const uploadedFiles = files.map((file) => ({
       file,
@@ -13,24 +9,15 @@ const handleUpload = (files) => {
     uploadedFiles.forEach(processXML);
     
   };
+
 /* Inicio processXML*/  
 const processXML = (uploadedFile) => {   
   console.log('aki 2')
-  console.log(uploadedFile);
-  const xml = uploadedFile.file;
-  const parseFromXML = (xml) => {
-    return new Promise((resolve, reject) => {
-      parseString(xml, { mergeAttrs: true, explicitArray: false}, function (err, ok) {
-        if (err) return resolve(err);
-        return resolve(ok);
-      });
-    });
-  };
-  
-  const data = parseFromXML(xml);
-  //const nf = await data.nfeProc.NFe.infNFe.ide.nNF;
-  console.log(data);
-
+  const dataFile = new FormData();
+  data.append('file', uploadedFile.file);
+  axios.post('/api/xmlcompras', dataFile).then(res => {
+    console.log(res.status);                         
+  });
 
 };
 
