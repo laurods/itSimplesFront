c9096c98a3553987e4f36effe8f88e43b8fef0e9
LAURO DE SA <itrafor@gmail.com>
2021-11-30 21:24:09 -0300

    xls 2

--- pages/api/xlsproducts.js
+++ pages/api/xlsproducts.js
@@ -2,11 +2,11 @@ import readXlsxFile from 'read-excel-file'
 
 export default async (req, res) => { 
   try {
-    if (req.file == undefined) {
-      return res.status(400).send("Please upload an excel file!");
-    }
+    // if (req.file == undefined) {
+    //   return res.status(400).send("Please upload an excel file!");
+    // }
 
-    return res.status(200).json({msg:'suscess'});
+    return res.status(200).json({msg:req.file.filename});
   } catch (error) {
     console.log(error);
     res.status(500).send({
