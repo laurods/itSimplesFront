a62b43a611e774de710faf69a80d1d4440e32cd2
LAURO DE SA <itrafor@gmail.com>
2021-12-04 08:38:55 -0300

    processProducts 4

--- services/processProducts.js
+++ services/processProducts.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 const processProducts = async (dataProducts) => {
   console.log('aki products');
   const cest = await axios.get('/api/productscest');
-  const listCest = cest.data;  // cest dos produtos substitutos, conforme site da sefaz rs
-  const productsSubstitutes = ([...listCest]) => { // compara todos os CEST dos produtos COMPRADOS com a tabela de produtos CEST
-    return dataProducts.filter(product => listCest.includes(product.cest));
+  const listCest = cest.data;  
+  const allCest = listCest.map((item) => item.cest); // cest dos produtos substitutos, conforme site da sefaz rs
+  const productsSubstitutes = ([...allCest]) => { // compara todos os CEST dos produtos COMPRADOS com a tabela de produtos CEST
+    return dataProducts.filter(product => allCest.includes(product.cest));
   }
 
   const filterProductdSubstitutes = productsSubstitutes(listCest); // chama a função
   console.log('lista cest');
   console.log(listCest)
+  console.log('all cest');
+  console.log(allCest)
   console.log('lista produtos');
   console.log(dataProducts)
   console.log('lista filtro');
