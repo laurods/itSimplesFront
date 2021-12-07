ce4fd4642277d522e2c4f1bcae8a8012b8f12c46
LAURO DE SA <itrafor@gmail.com>
2021-12-06 21:37:38 -0300

    add productsSubstitutes api 15

--- pages/api/purchasesSubstitutes.js
+++ pages/api/purchasesSubstitutes.js
@@ -10,38 +10,40 @@ const client = new MongoClient(url);
  module.exports = async (req, res) => {
     try {
 
-        const { filterProductdSubstitutes } = req.body; 
+        const { insertOne } = req.body; 
 
          await client.connect();
          const db = client.db(dbName);
          // Use the collection "people"
-         const col = db.collection("purchasesSubstitutes");        
-         filterProductdSubstitutes.forEach(e => {
-            col.updateOne(
-                { cean: e.cean },
-            {
-               $set: { 
-                 cean: e.cean,
-                 cest: e.cest,
-                 cfop: e.cfop,
-                 cnpjDestinatario: e.cnpjDestinatario,
-                 cnpjEmitente: e.cnpjEmitente,
-                 cofins: e.cofins,
-                 cst: e.cst,
-                 name: e.name,
-                 ncm: e.ncm,
-                 nf: e.nf, 
-                 pis: e.pis,
-                 vICMSST: e.vICMSST
+         const col = db.collection("purchasesSubstitutes");
+         const p = await col.insertOne(insertOne);
+         
+        //  filterProductdSubstitutes.forEach(e => {
+        //     col.updateOne(
+        //         { cean: e.cean },
+        //     {
+        //        $set: { 
+        //          cean: e.cean,
+        //          cest: e.cest,
+        //          cfop: e.cfop,
+        //          cnpjDestinatario: e.cnpjDestinatario,
+        //          cnpjEmitente: e.cnpjEmitente,
+        //          cofins: e.cofins,
+        //          cst: e.cst,
+        //          name: e.name,
+        //          ncm: e.ncm,
+        //          nf: e.nf, 
+        //          pis: e.pis,
+        //          vICMSST: e.vICMSST
                 
-                },				
+        //         },				
                
-            },
-            { upsert: true }
-            );
+        //     },
+        //     { upsert: true }
+        //     );
             
            
-       });
+       //});
 
          
          res.status(200).json({ msg: filterProductdSubstitutes });
