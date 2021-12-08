b5cda4adf3f4372fd197c5061f50a19a0ba31260
LAURO DE SA <itrafor@gmail.com>
2021-12-07 21:48:28 -0300

    insert 8

--- pages/api/purchasesSubstitutes.js
+++ pages/api/purchasesSubstitutes.js
@@ -11,18 +11,19 @@ const client = new MongoClient(url);
     try {
 
         //const { item } = req.body;
-        const  { item } = req.body;  
+        const  { item } = req.body;
+        const product = item;  
 
          await client.connect();
          const db = client.db(dbName);
          const col = db.collection("purchasesSubstitutes");
          //const p = await col.insertOne(item);
-        //  const p = await col.updateOne(
-        //      { cean: item.cean },
-        //      {
-        //         set: { 
-        //             cean: item.cean,
-        //             cest: item.cest,
+         const p = await col.updateOne(
+             { cean: product.cean },
+             {
+                set: { 
+                    cean: product.cean,
+                    cest: product.cest,
         //             cfop: cfop,
         //             cnpjDestinatario: cnpjDestinatario,
         //             cnpjEmitente: cnpjEmitente,
@@ -33,11 +34,11 @@ const client = new MongoClient(url);
         //             nf: item[0].nf, 
         //             pis: item[0].pis,
         //             vICMSST: item[0].vICMSST
-        //         },				
+                },				
                                    
-        //     },
-        //     { upsert: true }             
-        // );
+            },
+            { upsert: true }             
+        );
          
 
 
