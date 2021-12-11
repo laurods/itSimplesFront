ddfa03fcfd0a3c44659109fb5f4216d1fb342792
LAURO DE SA <itrafor@gmail.com>
2021-12-11 11:05:52 -0300

    agregate 9

--- pages/api/entradasbycnpj.js
+++ pages/api/entradasbycnpj.js
@@ -16,7 +16,7 @@ const client = new MongoClient(url);
          const col = db.collection("purchasesSubstitutes");
          const movimentos = await col.aggregate(
             [
-                { $match : { cnpjDestinatario: "$$cnpj" } },
+                { $match : { cnpjDestinatario: "04921325000168" } },
                 {
                     $addFields: {
                         vlrST: {$toDouble: "$vICMSST"}
