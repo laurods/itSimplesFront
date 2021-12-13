b0b351627293aed0a0fb251021194401fb31136c
LAURO DE SA <itrafor@gmail.com>
2021-12-13 20:20:51 -0300

    helpers  6

--- helpers/updateEntradasByCNPJ.js
+++ helpers/updateEntradasByCNPJ.js
@@ -2,16 +2,12 @@ import React, { useContext } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../contexts/AuthContext';
 
-function Update() {
-    const updateList = async () => {
+export default function Update() {
     const {activeCNPJ, setEntradasByCNPJ } = useContext(AuthContext);
+    const updateList = async () => {    
     const entradasByCNPJ = await axios.post('../pages/api/entradasbycnpj', { cnpj: activeCNPJ });        
     const listEntradasByCNPJ = entradasByCNPJ.data;
     setEntradasByCNPJ(listEntradasByCNPJ)
     }
     updateList();  
-};
-
-module.exports = {
-    Update
 };
