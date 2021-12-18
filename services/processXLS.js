fad251f16246d6f6d69fcbbc27ea2d8ede429a35
laurods <itrafor@gmail.com>
2021-12-18 10:12:51 -0300

    process xls 3

--- services/processXLS.js
+++ services/processXLS.js
@@ -1,8 +1,13 @@
+import readXlsxFile from 'read-excel-file'
 
 const processXLS = async (data) => {
-  const arrayXLS = data;
-  console.log(arrayXLS)  
-  
+  arrayXLS = await readXlsxFile(data[0]).then((rows) => {
+      console.log(rows)
+    // `rows` is an array of rows
+    // each row being an array of cells.
+  })
+ 
+ 
 };
 
 module.exports = {
