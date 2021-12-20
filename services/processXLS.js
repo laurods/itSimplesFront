d614a0fd6d95886de47bd2e92519ee23e24933fc
laurods <itrafor@gmail.com>
2021-12-20 20:16:36 -0300

    process xls 11

--- services/processXLS.js
+++ services/processXLS.js
@@ -5,7 +5,14 @@ const processXLS = async (data) => {
     console.log(data)
     
   const arrayXLS = await readXlsxFile(data).then((rows) => {
-   
+      const header = rows[0];
+      const movimento = header[4];
+      const cnpj = header[5];
+      console.log(movimento);
+      console.log(cnpj);
+      // console.log(rows[0]);
+      // skip header
+      rows.shift();
       const productSold = [];
       rows.forEach((row) => {
         let products = {
