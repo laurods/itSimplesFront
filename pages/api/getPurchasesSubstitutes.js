0c35486192775b031af3c17d56d87fc011e402bd
laurods <itrafor@gmail.com>
2021-12-20 22:08:59 -0300

    process xls 15

--- pages/api/getPurchasesSubstitutes.js
+++ pages/api/getPurchasesSubstitutes.js
@@ -12,25 +12,7 @@ const client = new MongoClient(url);
          await client.connect();         
          const db = client.db(dbName);
          const col = db.collection("purchasesSubstitutes");
-         const products = await col.find(
-             { },
-		    { projection: {
-                cean: 1,
-                cest: 0,
-                cfop: 0,
-                cnpjDestinatario: 0,
-                cnpjEmitente: 0,
-                cofins: 0,
-                movimento: 0,
-                name : 0,
-                ncm: 0,
-                nf: 0,
-                pis: 0,
-                vICMSST: 0,
-			   _id: 0
-            }
-			}
-         ).toArray();     
+         const products = await col.find({}).toArray();     
          
          res.status(200).json(products);
 
