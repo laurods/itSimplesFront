ed371ae602fd9dac2d8cf78dfad02f9b10c8af7d
laurods <itrafor@gmail.com>
2021-12-31 10:07:52 -0300

    calculete DAS4

--- services/processDAS.js
+++ services/processDAS.js
@@ -9,11 +9,20 @@ const calculateDAS = async (dataProducts, filterProductdSubstitutes) => {
         return sum + parseFloat(product.total);
     }, 0);
 
+    const dataDAS = {
+        movimento: this.dataProduct[0].movimento,
+        totalSalesST: totalSalesST.toFixed(2), 
+        totalSales: totalSales.toFixed(2)
+    }
     console.log('total sales')
     console.log(totalSales)
     
     console.log('total substitutes')
     console.log(totalSalesST)
+
+    console.log('dataDAS')
+    console.log(dataDAS)
+
     //window.location.reload() // atualiza a pagina
   }
 
