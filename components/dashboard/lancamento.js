aaff0e868aaaa43ec32a78277d55e6bf72b14cc0
laurods <itrafor@gmail.com>
2022-01-02 10:59:18 -0300

    mobile 0201_1

---
+++ components/dashboard/lancamento.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useContext } from 'react';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AuthContext } from '../../contexts/AuthContext';
+
+const theme = createTheme();
+export default function FormLancamento() {
+    const { addEmpresa } = useContext(AuthContext);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    const values = {
+        name: data.get('nome'),
+        cnpj: data.get('cnpj'),
+    }
+    
+    console.log(values);
+    addEmpresa(values);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component={Paper} maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+    
+          <Typography variant="h6" gutterBottom component="div">
+            Despesa
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: -1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="nome"
+              label="Nome"
+              name="nome"
+              autoComplete="none"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="cnpj"
+              label="VALOR"
+              type="cnpj"
+              id="cnpj"
+              autoComplete="none"
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Salvar
+            </Button>
+            
+          </Box>
+        </Box>
+       
+      </Container>
+    </ThemeProvider>
+  );
+}
---
+++ pages/mobile.js
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../contexts/AuthContext';
+import { parseCookies } from 'nookies';
+import Login from '../components/login/login.js'
+import Lancamento from '../components/dashboard/lancamento.js';
+import Content from '../components/dashboard/content.js';
+
+export default function Dashboard() {
+     const {setCNPJsByUsers, setActiveCNPJ, setMovimentosCNPJ, setEntradasByCNPJ, setDasByCNPJ, isAuthenticated } = useContext(AuthContext);
+     
+    useEffect(() => {
+      const loadAll = async() =>{
+        const cookies = parseCookies()
+        const clients = await axios.post('/api/cnpjbyuser', { user: cookies.idUser });        
+        const listClients = clients.data;
+        const movimentosByCNPJ = await axios.post('/api/movimentosbycnpj', { cnpj: listClients[0].cnpj });
+        const listMovimentos = movimentosByCNPJ.data;
+        const entradasByCNPJ = await axios.post('/api/entradasbycnpj', { cnpj: listClients[0].cnpj });        
+        const listEntradasByCNPJ = entradasByCNPJ.data;
+        const vendasByCNPJ = await axios.post('/api/salesBycnpj', { cnpj: listClients[0].cnpj });              
+        const listVendasByCNPJ = vendasByCNPJ.data;
+        const dasByCNPJ = await axios.post('/api/dasBycnpj', { cnpj: listClients[0].cnpj });              
+        const listDasByCNPJ = dasByCNPJ.data;        
+        setCNPJsByUsers(listClients)
+        setActiveCNPJ(listClients[0])
+        setMovimentosCNPJ(listMovimentos)
+        setEntradasByCNPJ(listEntradasByCNPJ)
+        setDasByCNPJ(listDasByCNPJ)
+        
+                
+      }
+      loadAll();
+    }, []);
+
+    return (
+      <>
+      {!isAuthenticated && <Login />}
+      {isAuthenticated && <Lancamento />}   
+      </>
+    );
+    
+}
