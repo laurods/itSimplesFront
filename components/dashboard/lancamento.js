99864a8fc418cdbc66c274142cee5c910a1b901b
laurods <itrafor@gmail.com>
2022-01-02 15:52:48 -0300

    mobile 0201_16

--- components/dashboard/lancamento.js
+++ components/dashboard/lancamento.js
@@ -34,7 +34,7 @@ export default function FormLancamento() {
     }   
         const values = {
             descricao,
-            valor,
+            valor : valor.replace(",", "."),
             tipo,
             }        
         console.log(values);
