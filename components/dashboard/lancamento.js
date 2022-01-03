7e1094a5c81a44e2aa078092b1ea8a0d820c1fff
laurods <itrafor@gmail.com>
2022-01-03 19:49:23 -0300

    mobile 0301_2

--- components/dashboard/lancamento.js
+++ components/dashboard/lancamento.js
@@ -38,7 +38,7 @@ export default function FormLancamento() {
         setValor(event.target.value);
     };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     //const data = new FormData(event.currentTarget);    
       //  setDescricao(data.get('descricao'))   
