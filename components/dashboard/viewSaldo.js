92199c4e605d78d98559011cbd07df7f99266175
laurods <itrafor@gmail.com>
2022-01-05 21:30:16 -0300

    mobile 0501_13

--- components/dashboard/viewSaldo.js
+++ components/dashboard/viewSaldo.js
@@ -38,6 +38,7 @@ export default function DasCNPJ() {
               <TableRow
                 key={row._id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                inputProps={{style: {fontSize: 20}}}
               >
                 <TableCell component="th" scope="row">
                   {row._id}
