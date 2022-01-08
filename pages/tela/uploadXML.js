59f9d1b1d072acf8d44c2ce4d319bcb507899268
laurods <itrafor@gmail.com>
2022-01-08 05:33:51 -0300

    content xml 0801_2

--- pages/tela/uploadXML.js
+++ pages/tela/uploadXML.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
-import Top from '../components/dashboard/top.js';
-import Content from '../components/dashboard/content.js';
+import Top from '../../components/dashboard/top.js';
+import Content from '../../components/dashboard/content.js';
 
 export default function UploadXML() {
     const { isAuthenticated } = useContext(AuthContext);
