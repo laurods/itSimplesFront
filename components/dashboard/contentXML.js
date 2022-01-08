2a737e967aa1bc06988549db3c50284eed509a09
laurods <itrafor@gmail.com>
2022-01-08 05:42:05 -0300

    content xml 0801_4

--- components/dashboard/contentXML.js
+++ components/dashboard/contentXML.js
@@ -4,11 +4,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import VendasByCNPJ from './vendasByCNPJ';
-import DasCNPJ from './dasCNPJ';
 import EntradasByCNPJ from './entradasByCNPJ';
-import FormEmpresa from './formEmpresa';
-import FormDespesa from './formDespesa';
-import FormEstoque from './formEstoque';
 import ViewProductsByNF from './viewProductsByNF'
 import UploadXML from './uploadXML';
 import UploadXLS from './uploadXLS';
@@ -39,18 +35,13 @@ export default function Content() {
         <Grid item xs={1}>
           {/* vazio */}
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={10}>
           <UploadXML uploadXML={uploadServiceXML}/>          
         </Grid>
         <Grid item xs={1}>
           {/* vazio */}
         </Grid>
-        <Grid item xs={4}>
-        <UploadXLS uploadXLS={uploadServiceXLS}/>
-        </Grid>
-        <Grid item xs={2}>
-          {/* vazio */}
-        </Grid>               
+                   
         
       </Grid>
       {/* NF E PRODUTOS */}
@@ -65,26 +56,8 @@ export default function Content() {
           {/* vazio */}
         </Grid>
        
-      </Grid>
-      
-    {/* RELATORIOS */}
-      <Grid container spacing={2}>
-        <Grid item xs={1}>
-          {/* vazio */}
-        </Grid>
-        <Grid item xs={5}>
-          <VendasByCNPJ />
-        </Grid>
-        <Grid item xs={1}>
-          {/* vazio */}
-        </Grid>
-        <Grid item xs={3}>
-          <EntradasByCNPJ />          
-        </Grid>              
-        <Grid item xs={2}>
-          {/* vazio */}
-        </Grid>
-      </Grid>
+      </Grid>      
+    
       
     </Box>
 
