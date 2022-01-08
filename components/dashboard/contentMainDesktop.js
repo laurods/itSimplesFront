128c27b0daca284ba564617b94bd9ee4d68d74a2
laurods <itrafor@gmail.com>
2022-01-08 06:17:54 -0300

    contentMain xml 0801_7

---
+++ components/dashboard/contentMainDesktop.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import VendasByCNPJ from './vendasByCNPJ';
+import EntradasByCNPJ from './entradasByCNPJ';
+import ViewProductsByNF from './viewProductsByNF'
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function Content() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>  
+      {/* NF E PRODUTOS */}
+      <Grid container spacing={2}>
+        <Grid item xs={1}>
+          {/* vazio */}
+        </Grid>
+        <Grid item xs={10}>
+        <ViewProductsByNF />
+        </Grid>
+        <Grid item xs={1}>
+          {/* vazio */}
+        </Grid>
+       
+      </Grid>
+      
+    {/* RELATORIOS */}
+      <Grid container spacing={2}>
+        <Grid item xs={1}>
+          {/* vazio */}
+        </Grid>
+        <Grid item xs={5}>
+          <VendasByCNPJ />
+        </Grid>
+        <Grid item xs={1}>
+          {/* vazio */}
+        </Grid>
+        <Grid item xs={3}>
+          <EntradasByCNPJ />          
+        </Grid>              
+        <Grid item xs={2}>
+          {/* vazio */}
+        </Grid>
+      </Grid>
+      
+    </Box>
+
+    
+  );
+}
