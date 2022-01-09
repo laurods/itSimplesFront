9327616d074c4fca182ccd3cd75bfd669167dc19
laurods <itrafor@gmail.com>
2022-01-09 06:15:36 -0300

    menu 0901_7

--- components/dashboard/viewProductsByNF.js
+++ components/dashboard/viewProductsByNF.js
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import CalculateIcon from '@material-ui/icons/Calculate';
+import CalculateIcon from '@mui/icons-material/Calculate';
 import PrintIcon from '@material-ui/icons/Print';
 import EditIcon from '@material-ui/icons/Edit';
 import SaveIcon from '@material-ui/icons/Save';
