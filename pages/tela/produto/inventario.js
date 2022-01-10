c2eba4c5775f991224b1c65a0d3f32a7b44c89ff
laurods <itrafor@gmail.com>
2022-01-10 19:55:44 -0300

    inventario 1001_2

--- pages/tela/produto/inventario.js
+++ pages/tela/produto/inventario.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { AuthContext } from '../../../contexts/AuthContext';
 import Login from '../../../components/login/login.js'
 import Top from '../../../components/dashboard/top.js';
-import Inventario from '../../../components/dashboard/inventario'
+import Inventario from '../../../components/dashboard/product/inventario'
 export default function Dashboard() {
      const {isAuthenticated, activeCNPJ, setProducts } = useContext(AuthContext);
      
