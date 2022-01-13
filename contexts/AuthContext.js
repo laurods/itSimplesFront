a368fc3cda5892a5595cadc19603506a91ed0ba2
laurods <itrafor@gmail.com>
2022-01-13 20:02:53 -0300

    tela cheia 1301_2

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -115,8 +115,18 @@ export function AuthProvider({ children }) {
     }
 
     const telaCheia = () => {        
-        const elem = document.fullscreenElement;
-        element.requestFullscreen()
+        const doc = window.document;
+        const docEl = doc.documentElement;
+      
+        const requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        const cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+      
+        if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+          requestFullScreen.call(docEl);
+        }
+        else {
+          cancelFullScreen.call(doc);
+        }
     }
 
 
