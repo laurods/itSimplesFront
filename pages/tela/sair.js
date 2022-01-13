0836a35217c7e942a3c448227b8395fba973b44b
laurods <itrafor@gmail.com>
2022-01-13 10:31:08 -0300

    sair 1301_2

--- pages/tela/sair.js
+++ pages/tela/sair.js
@@ -3,12 +3,10 @@ import { useRouter } from 'next/router';
 import { AuthContext } from '../../contexts/AuthContext';
 import TopMobile from '../../components/dashboard/topMobile';
 import PDV from '../../components/dashboard/pdvBasic';
-import Router from 'next/router';
 
 export default function Dashboard() {
   const { sair } = useContext(AuthContext);
   sair();
-  Router.push('../mobile'); 
 
     return (
       <>
