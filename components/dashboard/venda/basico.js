f856471ee68f4f63eaf5e543d2c6c281d218ec56
laurods <itrafor@gmail.com>
2022-01-14 04:31:32 -0300

    venda 1401_1

--- components/dashboard/venda/basico.js
+++ components/dashboard/venda/basico.js
@@ -7,7 +7,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { AuthContext } from '../../contexts/AuthContext';
+import { AuthContext } from '../../../contexts/AuthContext';
 import Alert from '@mui/material/Alert';
 import Grid from '@mui/material/Grid';
 
