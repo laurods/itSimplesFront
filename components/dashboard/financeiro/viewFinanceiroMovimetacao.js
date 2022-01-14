3a99f1be2e37d0c5b9799fb4fef203c96e6d3333
laurods <itrafor@gmail.com>
2022-01-14 06:47:25 -0300

    financeiro diario 1401_14

--- components/dashboard/financeiro/viewFinanceiroMovimetacao.js
+++ components/dashboard/financeiro/viewFinanceiroMovimetacao.js
@@ -6,8 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import PrintIcon from '@material-ui/icons/Print';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -16,9 +15,89 @@ import { AuthContext } from '../../../contexts/AuthContext';
 const theme = createTheme();
 
 export default function DasCNPJ() {
-    const { all } = useContext(AuthContext); 
+    const { all } = useContext(AuthContext);
+    const dataAtual = new Date();
+    const [day, setDay] = useState(dataAtual.getDate());
+    const [month, setMonth] = useState(dataAtual.getMonth() + 1);
+    const [year, setYear] = useState(dataAtual.getFullYear());
+    
+    const handleChangeDay = (event) => {
+        setDay(event.target.value);
+    };
+  
+    const handleChangeMonth = (event) => {
+      setMonth(event.target.value);
+  };
+  
+  const handleChangeYear = (event) => {
+    setYear(event.target.value);
+  };
+
+  const handleFilter = async (event) => {
+        console.log('filtrar')
+  }
   return (
-    <ThemeProvider theme={theme}>    
+    <ThemeProvider theme={theme}>
+        <Grid container spacing={2}>        
+        <Grid item xs={6} md={6}>
+          {/* datas */}
+          <TextField              
+              id="dia"
+              label="dia"
+              name="dia"
+              type="number"
+              value={day}
+              onChange={handleChangeDay}
+              autoComplete="off"
+              variant="outlined"
+              size="small"
+              sx={{                
+                maxWidth: '30%',
+              }}
+            />
+            <TextField              
+              id="mes"
+              label="mes"
+              name="mes"
+              type="number"
+              value={month}
+              onChange={handleChangeMonth}
+              autoComplete="off"
+              variant="outlined"
+              size="small"
+              sx={{                
+                maxWidth: '30%',
+              }}
+            />
+            <TextField              
+              id="ano"
+              label="ano"
+              name="ano"
+              type="number"
+              value={year}
+              onChange={handleChangeYear}
+              autoComplete="off"
+              variant="outlined"
+              size="small"
+              sx={{                
+                maxWidth: '35%',
+              }}
+            />
+            
+        </Grid>
+        <Grid item xs={6} md={6} >
+        <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              size="large"
+              onClick={handleFilter}              
+            >
+              Salvar
+            </Button>
+        </Grid>                    
+        
+      </Grid>    
 
       <TableContainer component={Paper} sx={{ mt: 2 }}>
       
@@ -26,7 +105,6 @@ export default function DasCNPJ() {
           <TableHead>
             <TableRow>
               <TableCell>Descricao</TableCell>
-              <TableCell>Tipo</TableCell>
               <TableCell>Valor</TableCell>
             </TableRow>
           </TableHead>
@@ -38,9 +116,6 @@ export default function DasCNPJ() {
               >
                 <TableCell component="th" scope="row">
                   {row.descricao}
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {row.tipo}
                 </TableCell>
                 <TableCell component="th" scope="row">
                   {(row.valor).toFixed(2)}
