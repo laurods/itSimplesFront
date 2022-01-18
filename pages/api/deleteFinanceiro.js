bb5b7fbe6d51749c2b94a4633414f5423d940660
laurods <itrafor@gmail.com>
2022-01-18 11:16:20 -0300

    delete 1801_11

--- pages/api/deleteFinanceiro.js
+++ pages/api/deleteFinanceiro.js
@@ -1,4 +1,4 @@
-const { MongoClient} = require("mongodb");
+const { MongoClient, ObjectId} = require("mongodb");
  
 // Replace the following with your Atlas connection string                                                                                                                                        
 const url = process.env.MONGODB_URI
