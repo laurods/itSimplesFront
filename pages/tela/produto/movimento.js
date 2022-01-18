dc4536e1ed7f8e1be1e97b9ce5a282e5f2a9f027
laurods <itrafor@gmail.com>
2022-01-18 16:27:07 -0300

    movimento 1801_2

--- pages/tela/produto/movimento.js
+++ pages/tela/produto/movimento.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TopMobile from '../../../components/dashboard/topMobile';
-import Movimento from '../../../components/dashboard/produto/movimento';
+import Movimento from '../../../components/dashboard/product/movimento';
 
 export default function Diario() {
   
