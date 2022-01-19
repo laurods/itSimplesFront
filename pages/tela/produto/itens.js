903adfc1888e853d758154624713e5e906a105ba
laurods <itrafor@gmail.com>
2022-01-18 21:14:38 -0300

    itens 1801_2

--- pages/tela/produto/itens.js
+++ pages/tela/produto/itens.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from '../../../contexts/AuthContext';
 import TopMobile from '../../../components/dashboard/topMobile';
 import Itens from '../../../components/dashboard/product/itens';
 
 export default function Diario() {
+
+    useEffect(() => {
+        const loadAll = async() =>{            
+          const allProductsBasics = await axios.get('/api/produtos/getAllProductsBasics');              
+          const listAllProductsBasics = allProductsBasics.data;
+          console.log('listAllProductsBasics');
+          console.log(listAllProductsBasics);
+        }
+        loadAll();
+      }, []);
   
     return (
       <>
