362326618c5b8d9a88a63deb49e311fc3f2cb904
laurods <itrafor@gmail.com>
2022-01-26 06:59:29 -0300

    itens 2601_7

--- components/dashboard/product/itens.js
+++ components/dashboard/product/itens.js
@@ -14,7 +14,6 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 import { AuthContext } from '../../../contexts/AuthContext';
 
 import axios from 'axios';
