244738d36d853a57a67d1447e6258f8799119c5f
laurods <itrafor@gmail.com>
2022-01-27 16:30:52 -0300

    pdv  2701_10

--- pages/tela/venda/pdv.js
+++ pages/tela/venda/pdv.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {  useState, useEffect } from 'react';
 import TopMobile from '../../../components/dashboard/topMobile';
 import Pdv from '../../../components/dashboard/venda/pdv';
 import { AuthContext } from '../../../contexts/AuthContext';
 import axios from 'axios';
 
-export default async function Diario() {
+export default function Diario() {    
   const { activeCNPJ } = useContext(AuthContext);
-    const dataProdutos = await axios.post('/api/produtos/getAllProductsBasics', { cnpj: activeCNPJ });
-    const listProdutos = dataProdutos.data;
-    console.log('listProdutos');
-    console.log(listProdutos);  
+  const [produtos, setProdutos] = useState([]);
+    useEffect(() => {
+        const loadAll = async() =>{            
+          const allProductsBasics = await axios.get('/api/produtos/getAllProductsBasics', { cnpj: activeCNPJ });              
+          const listAllProductsBasics = allProductsBasics.data;
+          console.log('listAllProductsBasics')
+          console.log(listAllProductsBasics)
+          setProdutos(listAllProductsBasics)
+        }
+        loadAll();
+      }, []);
+    
+     
   
     return (
       <>
