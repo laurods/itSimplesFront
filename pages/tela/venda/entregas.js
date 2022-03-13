1a777f66db65f5d4f66daab5e7e6dd26b4ef2ed8
laurods <itrafor@gmail.com>
2022-03-13 11:07:56 -0300

    entregas 13032022_2

---
+++ pages/tela/venda/entregas.js
@@ -0,0 +1,28 @@
+import React, {  useState, useEffect, useContext } from 'react';
+import TopMobile from '../../../components/dashboard/topMobile';
+import Pdv from '../../../components/dashboard/venda/pdv';
+import { AuthContext } from '../../../contexts/AuthContext';
+import axios from 'axios';
+
+export default function Diario() {    
+  const { activeCNPJ } = useContext(AuthContext);
+  const [produtos, setProdutos] = useState([]);
+    useEffect(() => {
+        const loadAll = async() =>{         
+          const allProductsBasics = await axios.post('/api/produtos/getAllProductsBasics', { cnpj: activeCNPJ });              
+          const listAllProductsBasics = allProductsBasics.data;
+          setProdutos(listAllProductsBasics)
+        }
+        loadAll();
+      }, []);
+    
+     
+  
+    return (
+      <>
+      <TopMobile />
+      <Pdv produtos={produtos}/>
+      </>
+    );
+    
+  }
