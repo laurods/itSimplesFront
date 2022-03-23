cdee9ced07e7b4764c674cb22b22311673d8ea0b
laurods <itrafor@gmail.com>
2022-03-22 21:50:25 -0300

    devices 2203_04

--- components/dashboard/device/viewMainDevice.js
+++ components/dashboard/device/viewMainDevice.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import EditIcon from '@material-ui/icons/Edit';
+import PrintIcon from '@material-ui/icons/Print';
+import SendIcon from '@material-ui/icons/Send';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 const theme = createTheme();
 
 export default function ViewMainMobile() {
+    const {
+        devices,
+        CNPJsByUsers,
+        activeCNPJ,       
+        isAuthenticated,
+        } = useContext(AuthContext);
     
   return (
     <ThemeProvider theme={theme}>
@@ -12,7 +32,34 @@ export default function ViewMainMobile() {
       <Grid container spacing={2}>
         <Grid item xs={1} md={1}>
         </Grid>
-        <Grid item xs={3} md={3}>           
+        <Grid item xs={3} md={3}>
+        <TableContainer component={Paper} sx={{ mt: 2 }}>
+        
+        <Table sx={{ minWidth: 200 }} aria-label="simple table">
+            <TableHead>                    
+            </TableHead>
+            <TableBody sx={{ fontSize: 45, fontWeight: 'medium' }}>
+            {CNPJsByUsers.map((row) => (
+                <TableRow
+                key={row.cnpj}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}                                       
+                >
+                <TableCell component="th" scope="row">
+                            <Button          
+                            fullWidth
+                            size="large" 
+                            variant="contained"                            
+                            >
+                                {row.name}
+                            </Button>
+                        </TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+    </TableContainer>
+
+
         </Grid>
         <Grid item xs={7} md={7}>           
         </Grid>
