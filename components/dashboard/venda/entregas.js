fe3427e0731fae967026771f8094199197c00b37
laurods <itrafor@gmail.com>
2022-04-15 03:50:30 -0300

    entrega  1504_13

--- components/dashboard/venda/entregas.js
+++ components/dashboard/venda/entregas.js
@@ -85,14 +85,20 @@ export default function Entregas(props) {
     const handleBairro = (event) => { setBairro(event.target.value) };
     const handlePedido = (event) => { setPedido(event.target.value) };
     const handleTxEntrega = (event) => {
-       let vlrEntrega = event.target.value.replace(",", ".");
-       console.log(vlrEntrega)
-       console.log('vlrEntrega')
-       let vEntrega = parseFloat(vlrEntrega);
-       console.log(vEntrega)
-       console.log('vEntrega')
-      setTxEntrega(vEntrega)
-      setValorPagar(txEntrega+valorPedido) 
+      if(event.target.value.length != 0){
+        let vlrEntrega = event.target.value.replace(",", ".");
+        console.log(vlrEntrega)
+        console.log('vlrEntrega')
+        let vEntrega = parseFloat(vlrEntrega);
+        console.log(vEntrega)
+        console.log('vEntrega')
+        setTxEntrega(vEntrega)
+        setValorPagar(txEntrega+valorPedido) 
+      }else{
+        let entrega = 0;
+        setTxEntrega(entrega)
+      }
+      
     };
     const handleValorPedido = (event) => {
       let vlrPedido = event.target.value.replace(",", ".");
