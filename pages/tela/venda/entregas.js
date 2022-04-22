04e5ec4a78d9b92561c5249eb0895fe7f0bd518a
laurods <itrafor@gmail.com>
2022-04-22 11:41:44 -0300

    entrega  2204_16

--- pages/tela/venda/entregas.js
+++ pages/tela/venda/entregas.js
@@ -1,6 +1,7 @@
 import React, {  useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../../../contexts/AuthContext';
 import Entregas from '../../../components/dashboard/venda/entregas';
+import Login from '../../../components/login/login.js'
 import axios from 'axios';
 
 const fetchData = async () => await axios.get('https://it-simples-front.vercel.app/api/consumidores/getAll')
