e3861bfd3e0b175092e1e09e13bbd00b8bc2bd4e
laurods <itrafor@gmail.com>
2022-04-22 13:51:31 -0300

    entrega  2204_22

--- pages/tela/venda/entregas.js
+++ pages/tela/venda/entregas.js
@@ -27,9 +27,12 @@ export default function Diario({  consumers, error  }) {
       const clients = await axios.post('../../api/cnpjbyuser', { user: cookies.idUser });        
       const listClients = clients.data;
       setCNPJsByUsers(listClients)
-      setActiveCNPJ(listClients[0].cnpj)
+      console.log('cookies.idUser')
+      console.log(cookies.idUser)
       console.log('listClients')
       console.log(listClients)
+      //setActiveCNPJ(listClients[0].cnpj)
+      
     }
     loadAll();
   }, []);
