e1ccaee8e3c8c392bdbc42047a283ad179350006
laurods <itrafor@gmail.com>
2022-05-17 15:17:56 -0300

    tenant  1705_6

--- components/tenant/header.js
+++ components/tenant/header.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import styles from '../../styles.module.css';
+import styles from '../../tenant.module.css';
 
 export default({black})=>{
     return(
         <header className={styles.myHeader}>
-            <div className='header--logo'>
+            <div className={styles.headerLogo}>
                 {/* <a href="/">
                     <img src="/images/logobaggio960_960.png" alt="Logo Baggio Festas" />
                 </a> */}
             </div>
-            <div className="header--seach">
+            <div className={styles.headerSeach}>
                 <input id="seach" type="text" placeholder="Encontre seu produto na Baggio" />
             </div>
 
-            <div className="header--user">
+            <div className={styles.headerUser}>
                 <a href="/">
                     <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Usuário" />
                    
