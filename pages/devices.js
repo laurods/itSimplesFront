e1d31ed32258e02e0564a9cbd5888fbb046e0a93
laurods <itrafor@gmail.com>
2022-05-28 16:41:12 -0300

    devices 2805_3

--- pages/devices.js
+++ pages/devices.js
@@ -1,7 +1,7 @@
 import React, {  useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import axios from 'axios';
-import Seach from '../components/dashboard/device/search'
+import Seach from '../components/dashboard/device/seach'
 
 
 
