55a07d6e718ed8c0e8ba1f7a4f701c40e23a3de6
laurods <itrafor@gmail.com>
2022-05-29 09:30:28 -0300

    devices 2905_4

--- components/dashboard/device/topSearch.js
+++ components/dashboard/device/topSearch.js
@@ -38,23 +38,7 @@ export default function TopSearch(props) {
       <Box sx={{ flexGrow: 1, mt: 2 }}>
 
       <Grid container spacing={2} sx={{ mt: 3 }}>   
-        <Grid item xs={6} md={6}>
-         <TextField
-              margin="normal"
-              required
-              autoFocus
-              inputProps={{style: {fontSize: 40}}}
-              fullWidth
-              name="serial"
-              label="Serial | IMEI"
-              id="serial"
-              type="number"
-              value={serial}
-              onChange={handleTextSearch}                      
-              autoComplete="off"
-              variant="standard"
-            />
-        </Grid>
+        
 
         <Grid item xs={6} md={6}>
          <TextField
@@ -64,10 +48,58 @@ export default function TopSearch(props) {
               inputProps={{style: {fontSize: 40}}}
               fullWidth
               name="filial"
-              label="CNPJ | Cidade"
+              label="SERIAL| CIDADE| CNPJ"
               id="filial"
               value={textSearch}
               onChange={handleTextSearch}            
+              autoComplete="off"
+              variant="standard"
+            />
+        </Grid>
+
+        <Grid item xs={2} md={2}>
+         <TextField
+              margin="normal"
+              disabled              
+              inputProps={{style: {fontSize: 40}}}
+              fullWidth
+              name="proprios"
+              label="QUANT. PROPRIOS"
+              id="proprios"
+              type="number"
+              value="0"                     
+              autoComplete="off"
+              variant="standard"
+            />
+        </Grid>
+
+         <Grid item xs={2} md={2}>
+         <TextField
+              margin="normal"
+              disabled              
+              inputProps={{style: {fontSize: 40}}}
+              fullWidth
+              name="locados"
+              label="QUANT. LOCADOS"
+              id="locados"
+              type="number"
+              value="0"                     
+              autoComplete="off"
+              variant="standard"
+            />
+        </Grid>
+
+        <Grid item xs={2} md={2}>
+         <TextField
+              margin="normal"
+              disabled              
+              inputProps={{style: {fontSize: 40}}}
+              fullWidth
+              name="valor"
+              label="VALOR LOCAÇÃO"
+              id="valor"
+              type="number"
+              value="0"                     
               autoComplete="off"
               variant="standard"
             />
