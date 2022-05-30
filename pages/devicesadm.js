9a7b9830f0860f68f769c303b549037b5804a073
laurods <itrafor@gmail.com>
2022-05-30 20:29:20 -0300

    devices 3005_26

---
+++ pages/devicesadm.js
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../contexts/AuthContext';
+import { parseCookies } from 'nookies';
+import Login from '../components/login/login.js'
+import ListDevicesMain from '../components/dashboard/device/listDevicesMain';
+
+
+export default function Devices() {
+     const {setIsUserADM, isAuthenticated, isUserADM } = useContext(AuthContext);
+     
+    useEffect(() => {
+      const loadAll = async() =>{
+        const cookies = parseCookies()
+        if(cookies.roleUser === "adm") {
+          setIsUserADM(true)
+        }                
+      }
+      loadAll();
+    }, []);
+
+    return (
+      <>
+      {!isAuthenticated && <Login />}      
+      {isUserADM && <ListDevicesMain />}  
+      </>
+    );
+    
+}
