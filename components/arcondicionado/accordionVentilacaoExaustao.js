185a347d2d48ea8a242a1b9bb560ad65b035ed39
laurods <itrafor@gmail.com>
2022-06-27 15:18:36 -0300

    ar 270622_15

--- components/arcondicionado/accordionVentilacaoExaustao.js
+++ components/arcondicionado/accordionVentilacaoExaustao.js
@@ -3,6 +3,7 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function AccordionVentilacaoExaustao() {
   return (
