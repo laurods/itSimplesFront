34d064d94f26714dbab392ee3847d6e8416197bf
laurods <itrafor@gmail.com>
2022-06-27 13:35:56 -0300

    ar 270622_3

--- components/arcondicionado/main.js
+++ components/arcondicionado/main.js
@@ -11,9 +11,19 @@ export default function ViewMainMobile() {
     <ThemeProvider theme={theme}>
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={12}>
-             <div>....</div>
+        <Grid item xs={4} md={4}>
+             <div>Sistema de Ventilação/Exaustão</div>
         </Grid>
+        <Grid item xs={4} md={4}>
+             <div>Sistema de Ar Condicionado</div>
+        </Grid>
+        <Grid item xs={4} md={4}>
+             <div>Indiretos</div>
+        </Grid>
+        <Grid item xs={4} md={4}>
+             <div>Resumo Custos Indiretos</div>
+        </Grid>
+        
 
       </Grid>
     </Box>
