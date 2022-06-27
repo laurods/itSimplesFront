aa9c3b4ade5ff66d4b3420a12473dfa5095106ab
laurods <itrafor@gmail.com>
2022-06-27 16:03:40 -0300

    ar 270622_19

---
+++ components/arcondicionado/tableVentilacaoExaustao.js
@@ -0,0 +1,52 @@
+import React, { useContext, useEffect, useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import EditIcon from '@material-ui/icons/Edit';
+
+export default function TableVentilacaoExaustao() {
+    const items = [
+        {id: 1, name: 'VENTILADORES'},
+        {id: 2, name:'FILTRAGEM'},
+        {id: 3, name:'COIFAS E CAPTORES'},
+        {id: 4, name:'FILTROS'},
+        {id: 5, name:'CORTINA DE AR'},
+        {id: 6, name:'AMORTECEDORES DE VIBRAÇÃO'},
+        {id: 7, name:'SUPORTES'},
+        {id: 8, name:'DUTOS EM CHAPA'},
+        {id: 9, name:'DUTOS EM MPU'},
+        {id: 10, name:'OUTROS'},
+        {id: 11, name:'DIFUSÃO DE AR'},
+        {id: 12, name:'PAINEIS ELETRICOS'},
+        {id: 13, name:'REDES ELETRICAS'},
+     ]
+   
+  return (   
+            <TableContainer component={Paper} sx={{ mt: 2 }}>
+        
+                <Table sx={{ minWidth: 200 }} aria-label="simple table">                   
+                    <TableBody sx={{ fontSize: 45, fontWeight: 'medium' }}>
+                    {items.map((row) => (
+                        <TableRow
+                        key={row.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}                
+                        >
+                        <TableCell component="th" scope="row">
+                            {row.name}
+                        </TableCell>
+                        
+                        <TableCell component="th" scope="row">
+                            <EditIcon />
+                        </TableCell>
+
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>            
+     
+  );
+}
--- pages/ar/ventilacaoExaustao.js
+++ pages/ar/ventilacaoExaustao.js
@@ -4,38 +4,20 @@ import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Top from '../../components/arcondicionado/top';
-import AccordionVentilacaoExaustao from '../../components/arcondicionado/accordionVentilacaoExaustao';
+import TableVentilacaoExaustao from '../../components/arcondicionado/tableVentilacaoExaustao';
+
 
 const theme = createTheme();
 
 export default function ventilacaoExaustao() {
-    const items = [
-        {name: 'VENTILADORES'},
-        {name:'FILTRAGEM'},
-        {name:'COIFAS E CAPTORES'},
-        {name:'FILTROS'},
-        {name:'CORTINA DE AR'},
-        {name:'AMORTECEDORES DE VIBRAÇÃO'},
-        {name:'SUPORTES'},
-        {name:'DUTOS EM CHAPA'},
-        {name:'DUTOS EM MPU'},
-        {name:'OUTROS'},
-        {name:'DIFUSÃO DE AR'},
-        {name:'PAINEIS ELETRICOS'},
-        {name:'REDES ELETRICAS'},
-     ]
+   
     
   return (
     <ThemeProvider theme={theme}>
         <Top />
         <Container>             
             <Box sx={{ flexGrow: 1, mt: 2 }}>
-                {
-                    items.map(item => {
-                        <AccordionVentilacaoExaustao name={item.name}/>
-                    })
-                }
-                
+                <TableVentilacaoExaustao />
             </Box>
         </Container>
     </ThemeProvider>
