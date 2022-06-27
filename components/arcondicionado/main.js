03bd0be26d1f8ed29115bcdcc0740f9838f406a9
laurods <itrafor@gmail.com>
2022-06-27 14:15:28 -0300

    ar 270622_7

--- components/arcondicionado/main.js
+++ components/arcondicionado/main.js
@@ -10,57 +10,58 @@ export default function ViewMainMobile() {
     
   return (
     <ThemeProvider theme={theme}>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={6}>
-            <Button          
-            fullWidth
-            size="large" 
-            variant="contained"
-            color="success" 
-            sx={{ mt: 5 }}
-           >
-                Ventilação/Exaustão
-            </Button>
-        </Grid>
-        <Grid item xs={6} md={6}>
-            <Button          
-                fullWidth
-                size="large" 
-                variant="contained"
-                color="primary" 
-                sx={{ mt: 5 }}
-            >
-                    Ventilação/Exaustão
-            </Button>
-        </Grid>
-        <Grid item xs={6} md={6}>
-            <Button          
-                fullWidth
-                size="large" 
-                variant="outlined"
-                color="error" 
-                sx={{ mt: 5 }}
-            >
-                    Custos Indiretos
-            </Button>
-        </Grid>
-        <Grid item xs={6} md={6}>
-            <Button          
-                fullWidth
-                size="large" 
-                variant="outlined"
-                color="error" 
-                sx={{ mt: 5 }}
-            >
-                    Resumo Custos Indiretos
-            </Button>
-        </Grid>
-        
+        <Container> 
+            <Box sx={{ flexGrow: 1, mt: 2 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={6} md={6}>
+                        <Button          
+                        fullWidth
+                        size="large" 
+                        variant="contained"
+                        color="success" 
+                        sx={{ mt: 5 }}
+                    >
+                            Ventilação/Exaustão
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} md={6}>
+                        <Button          
+                            fullWidth
+                            size="large" 
+                            variant="contained"
+                            color="primary" 
+                            sx={{ mt: 5 }}
+                        >
+                                Ventilação/Exaustão
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} md={6}>
+                        <Button          
+                            fullWidth
+                            size="large" 
+                            variant="outlined"
+                            color="error" 
+                            sx={{ mt: 5 }}
+                        >
+                                Custos Indiretos
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6} md={6}>
+                        <Button          
+                            fullWidth
+                            size="large" 
+                            variant="outlined"
+                            color="error" 
+                            sx={{ mt: 5 }}
+                        >
+                                Resumo Custos Indiretos
+                        </Button>
+                    </Grid>
+                    
 
-      </Grid>
-    </Box>
-
+                </Grid>
+            </Box>
+        </Container>
     </ThemeProvider>
   );
 }
