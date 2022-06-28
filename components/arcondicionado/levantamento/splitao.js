59d6671e405cead9231510c1ec76fa47835dbe49
laurods <itrafor@gmail.com>
2022-06-28 13:32:09 -0300

    ar 280622_13

--- components/arcondicionado/levantamento/splitao.js
+++ components/arcondicionado/levantamento/splitao.js
@@ -14,104 +14,55 @@ import Grid from '@mui/material/Grid';
 
 const theme = createTheme();
 export default function TelaLevantamento(props) {
-    const [tag, setTag] = useState(''); 
+    const [tag, setTag] = useState('');
+    const [capacidadetr, setCapacidadetr] = useState(''); 
 
   return (
     <ThemeProvider theme={theme}>
       <Container>      
       <Box sx={{ flexGrow: 1, mt: 2 }}>
 
-      <Grid container spacing={2} sx={{ mt: 3 }}>
-      
-         <Grid item xs={12} md={5}>
-          <TextField
-            margin="normal"
-            required
-            inputProps={{style: {fontSize: 40}}}
-            label="TAG"
-            id="tag"
-            onChange={(event => setTag(event.target.value.toUpperCase()))}
-            value={tag}
-            fullWidth
-            autoComplete='off'
-            variant="standard"
-          />
-        </Grid>
-        
-      
-       {/* <Grid item xs={9} md={4}>
-          <TextField
-            label="Rua"
-            id="rua"
-            onChange={handleRua}
-            value={rua}
-            fullWidth
-            autoComplete='off'
-            variant="outlined"
-          />
-        </Grid>
-      
-
-      <Grid item xs={3} md={1}>
-          <TextField
-            label="Numero"
-            id="numero"
-            type="number"
-            onChange={handleNumero}
-            value={numero}
-            fullWidth
-            variant="outlined"
-          />
-        </Grid>
-      
-        <Grid item xs={6} md={2}>
-          <TextField
-            label="Complemento"
-            id="complemento"
-            onChange={handleComplemento}
-            value={complemento} 
-            fullWidth
-            autoComplete='off'
-            variant="outlined"
-          />
-        </Grid>
-        
-        <Grid item xs={6} md={3}>
-          <TextField
-            label="Bairro" 
-            id="bairro"
-            onChange={handleBairro}
-            value={bairro}
-            fullWidth
-            autoComplete='off'
-            variant="outlined"
-          />
-        </Grid>
-      
-       <Grid item xs={4} md={1}>
-          <TextField            
-            label="Entrega"
-            id="txEntrega"
-            type="number"
-            onChange={handleTxEntrega}
-            value={txEntrega}
-            fullWidth
-            variant="outlined"
-          />
-        </Grid>
-         <Grid item xs={12} md={1}>
-          <Button          
-            fullWidth
-            size="large" 
-            variant="contained" 
-            sx={{ mt: 1 }}
-            onClick={handleView}
-            endIcon={<SaveIcon />}
-            >
-                   
-          </Button>             
-         </Grid>        */}
-           
+      <Grid container spacing={2} sx={{ mt: 3 }}>      
+            <Grid item xs={1} md={1}>
+            <TextField
+                margin="normal"
+                required
+                inputProps={{style: {fontSize: 40}}}
+                label="TAG"
+                id="tag"
+                onChange={(event => setTag(event.target.value.toUpperCase()))}
+                value={tag}
+                fullWidth
+                autoComplete='off'
+                variant="outlined"
+            />
+            </Grid>
+            <Grid item xs={1} md={1}>
+            <TextField
+                margin="normal"
+                required
+                inputProps={{style: {fontSize: 40}}}
+                label="Capacidade TR"
+                id="capacidadetr"
+                onChange={(event => setCapacidadetr(event.target.value.toUpperCase()))}
+                value={tag}
+                fullWidth
+                autoComplete='off'
+                variant="outlined"
+            />
+            </Grid>
+            <Grid item xs={1} md={1}>
+            <Button          
+                fullWidth
+                size="large" 
+                variant="contained" 
+                sx={{ mt: 1 }}
+                onClick={handleView}
+                endIcon={<SaveIcon />}
+                >
+                    
+            </Button>             
+            </Grid> 
         </Grid>
         
       </Box>      
