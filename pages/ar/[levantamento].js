2be4c4f9b24a9bf3db3f2c41b1535929b645185a
laurods <itrafor@gmail.com>
2022-06-28 11:19:16 -0300

    ar 280622_10

--- pages/ar/[levantamento].js
+++ pages/ar/[levantamento].js
@@ -1,17 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import { useRouter } from 'next/router'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Top from '../../components/arcondicionado/top';
 
 
 export default function Levantamento() {
   const router = useRouter()
   const { levantamento } = router.query
-  console.log(levantamento);
 
   return (
-    <>    
-    <div> levantamento de: {levantamento} </div>
-    
-    </>
+    <ThemeProvider theme={theme}>
+        <Top />
+        <Container>             
+            <Box sx={{ flexGrow: 1, mt: 2 }}>
+            <TextField id="outlined-basic" label="Tela" variant="outlined" defaultValue={`Levantamento de ${levantamento}`} disabled />            
+            </Box>
+        </Container>
+    </ThemeProvider>
   );
+
 }
 
