498e6d90910d309f71a32031f70b036b4a8826df
laurods <itrafor@gmail.com>
2022-06-28 11:06:52 -0300

    ar 280622_8

--- pages/ar/[levantamento].js
+++ pages/ar/[levantamento].js
@@ -2,13 +2,14 @@ import React, {useEffect, useState} from 'react';
 import { useRouter } from 'next/router'
 
 
-export default function Tenant() {
+export default function Levantamento() {
   const router = useRouter()
   const { levantamento } = router.query
+  console.log(levantamento);
 
   return (
     <>    
-    <div> levantamento de:  ${levantamento}</div>
+    <div> levantamento de:  </div>
     
     </>
   );
