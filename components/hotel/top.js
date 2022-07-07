e08fde9bd81acf8e4336ea926337b87d843535ba
laurods <itrafor@gmail.com>
2022-07-07 18:22:30 -0300

    hotel 070722

---
+++ components/hotel/top.js
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import { AuthContext } from '../../contexts/AuthContext';
+
+
+export default function Top() {
+    const { telaCheia } = useContext(AuthContext);
+    
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <AccountBalanceWalletIcon onClick={telaCheia}/>
+
+          </IconButton>                  
+          <Typography variant="h5" gutterBottom component="div" sx={{ flexGrow: 1 }}>          
+           Blue Tree Hotels
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid item xs={12} md={6}>
+        <Card>
+            <CardContent sx={{ flex: 1 }}>
+                <Typography variant="subtitle2" component="div">
+                Olá, ficamos muito felizes em ter você com a gente!
+                </Typography>
+                <Typography variant="body2">
+                Gostaríamos de saber como foi sua experiencia. 
+                <br />
+                Ouvir você nos ajuda a aprimorar o seu atendimento, sempre!
+                <br />
+                São apenas 8 perguntas que você pode responder em menos de 1 minuto.                
+                </Typography>
+               
+            </CardContent>
+        </Card>
+      </Grid>
+    </Box>
+  );
+}
+
+
---
+++ pages/hotel/[id].js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import { useRouter } from 'next/router'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Top from '../../components/hotel/top';
+
+const theme = createTheme();
+
+export default function Levantamento() {
+  const router = useRouter()
+  const { id } = router.query
+  console.log(id)
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Top />
+        <Container>                         
+            <Box sx={{ flexGrow: 1, mt: 2 }}>            
+                        
+            </Box>
+        </Container>
+    </ThemeProvider>
+  );
+
+}
+
