bd6e46c214d8745deefe863ce0cc761f086329c0
laurods <itrafor@gmail.com>
2022-07-15 19:54:37 -0300

    hotel 150722_28

--- components/hotel/dashboard.js
+++ components/hotel/dashboard.js
@@ -1,21 +1,13 @@
 import React, {  useState, useEffect, useContext } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import CircularProgress from '@mui/material/CircularProgress';
 import { makeStyles, } from '@material-ui/core/styles';
 import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import axios from 'axios';
-import Chart1 from './chart1';
 
 export default function Dashboard({dataQuizz}) {      
     const totalNo = dataQuizz.reduce((sum, item) => sum + item.answerNoCount, 0);
-    const totalYes = dataQuizz.reduce((sum, item) => sum + item.answerYesCount, 0);   
-    console.log(totalNo)
-    console.log(totalYes)
-    console.log(totalNo + totalYes )
+    const totalYes = dataQuizz.reduce((sum, item) => sum + item.answerYesCount, 0);
 
     const useStyles = makeStyles(theme => ({
         root: {
@@ -26,15 +18,14 @@ export default function Dashboard({dataQuizz}) {
 
     const classes = useStyles();
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      
+    <Box sx={{ width: '100%' }}>      
       <Grid container spacing={2}>
-        <Grid item xs={12}>            
+        <Grid item xs={12}>
+                    
           <p>Sim {Math.round(((totalYes)/(totalNo + totalYes))*100)} %</p>          
           <LinearProgress 
           variant="determinate"
           color ="primary"
-          valueBuffer={42}
           value={totalYes} 
           classes={{
             root: classes.root,
@@ -46,16 +37,12 @@ export default function Dashboard({dataQuizz}) {
           <LinearProgress 
           variant="determinate"
           color="secondary"
-          valueBuffer={42} 
           value={totalNo} 
           classes={{
             root: classes.root,
           }}
           />          
         </Grid>
-        
-                     
-        
       </Grid>
       
     </Box>
