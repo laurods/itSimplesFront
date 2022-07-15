09b81c5e6d28e23255ec025f009f1762a13cffe6
laurods <itrafor@gmail.com>
2022-07-15 17:36:46 -0300

    hotel 150722_7

--- components/hotel/dashboard.js
+++ components/hotel/dashboard.js
@@ -1,9 +1,11 @@
 import React, {  useState, useEffect, useContext } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import axios from 'axios';
+import Chart1 from './chart1';
 
 export default function Dashboard({dataQuizz}) {
     console.log('dataQuizz')
@@ -20,6 +22,7 @@ export default function Dashboard({dataQuizz}) {
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <p>Dashboard</p>
+          <Chart1 />
         </Grid>
                      
         
