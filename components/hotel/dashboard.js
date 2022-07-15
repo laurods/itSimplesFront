3ceb3f88034d50ad0dacfced3b067d102348d4c3
laurods <itrafor@gmail.com>
2022-07-15 18:08:42 -0300

    hotel 150722_11

--- components/hotel/dashboard.js
+++ components/hotel/dashboard.js
@@ -8,17 +8,17 @@ import axios from 'axios';
 import Chart1 from './chart1';
 
 export default function Dashboard({dataQuizz}) {
-    const [chat1Yes, setChat1Yes] = useState('');
-    const [chat1No, setChat1No] = useState('');
-    const [chat1Total, setChat1Total] = useState('');
-    const totalNo = dataQuizz.reduce((sum, item) => sum + item.answerNoCount, 0);
-    const totalYes = dataQuizz.reduce((sum, item) => sum + item.answerYesCount, 0);
-    setChat1Total(totalNo + totalYes )
-    setChat1No(totalNo)
-    setChat1Yes(totalYes)
-    console.log(chat1Yes)
-    console.log(chat1No)
-    console.log(chat1Total)
+    // const [chat1Yes, setChat1Yes] = useState('');
+    // const [chat1No, setChat1No] = useState('');
+    // const [chat1Total, setChat1Total] = useState('');
+    // const totalNo = dataQuizz.reduce((sum, item) => sum + item.answerNoCount, 0);
+    // const totalYes = dataQuizz.reduce((sum, item) => sum + item.answerYesCount, 0);
+    // setChat1Total(totalNo + totalYes )
+    // setChat1No(totalNo)
+    // setChat1Yes(totalYes)
+    // console.log(chat1Yes)
+    // console.log(chat1No)
+    // console.log(chat1Total)
     
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -26,11 +26,7 @@ export default function Dashboard({dataQuizz}) {
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <p>Dashboard</p>
-          {/* <Chart1 
-          yes={chat1Yes}
-          no={chat1No}
-          total={chat1Total}
-          /> */}
+          
         </Grid>
                      
         
