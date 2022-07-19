f6698008cc6950888fefed890682e221b304e9b7
laurods <itrafor@gmail.com>
2022-07-19 09:40:24 -0300

    hotel 190722_2

--- components/hotel/chart1.js
+++ components/hotel/chart1.js
@@ -3,11 +3,11 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { makeStyles, } from '@material-ui/core/styles';
 import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
 
 export default function Chat1({dataQuizz}) {      
     const totalNo = dataQuizz.reduce((sum, item) => sum + item.answerNoCount, 0);
     const totalYes = dataQuizz.reduce((sum, item) => sum + item.answerYesCount, 0);
-
     const useStyles = makeStyles(theme => ({
         root: {
           height: theme.spacing(4),
@@ -19,11 +19,15 @@ export default function Chat1({dataQuizz}) {
       }));
 
     const classes = useStyles();
+    console.log('dataQuizz')
+    console.log(dataQuizz)
   return (
     <Box className={classes.box}>      
       <Grid container spacing={2}>
         <Grid item xs={12}>
-                    
+          <Typography variant="subtitle1" component="div">          
+          
+          </Typography>                    
           <span>Sim {Math.round(((totalYes)/(totalNo + totalYes))*100)} %</span>          
           <LinearProgress 
           variant="determinate"
