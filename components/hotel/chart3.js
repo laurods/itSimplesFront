512ad9e54c038fdb624b41e727827f1d5d36eceb
laurods <itrafor@gmail.com>
2022-07-19 12:29:28 -0300

    hotel 190722_25

---
+++ components/hotel/chart3.js
@@ -0,0 +1,66 @@
+import React, {  useState, useEffect, useContext } from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { makeStyles, } from '@material-ui/core/styles';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+
+export default function Chat3({dataQuizz}) { 
+    const useStyles = makeStyles(theme => ({
+        root: {
+          height: theme.spacing(4),
+          backgroundColor: theme.palette.background.default,
+        },
+        box: {
+            backgroundColor: theme.palette.background.default,
+          },
+      }));
+    
+    const qYes = [];
+    const qNo = [];
+    const classes = useStyles();
+    dataQuizz.map(({quizzes})=>{      
+      quizzes.map(({question, answer})=>{        
+        if(question === 'Registro de reserva estava correto.' && answer ==='Sim') {
+          qYes.push(answer)
+        }
+        if(question === 'Registro de reserva estava correto.' && answer ==='Não') {
+          qNo.push(answer)
+        }
+      })      
+      });
+   
+    
+  return (
+    <Box className={classes.box}>      
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="subtitle1" component="div">          
+          Registro de reserva estava correto.
+          </Typography>                    
+          <span>Sim {Math.round(((qYes.length)/(qYes.length + qNo.length))*100)} %</span>          
+          <LinearProgress 
+          variant="determinate"
+          color ="primary"
+          value={Math.round(((qYes.length)/(qYes.length + qNo.length))*100)} 
+          classes={{
+            root: classes.root,
+          }}          
+          />
+          <span>Não {Math.round(((qNo.length)/(qYes.length + qNo.length))*100)} %</span>
+          <LinearProgress 
+          variant="determinate"
+          color="secondary"
+          value={Math.round(((qNo.length)/(qYes.length + qNo.length))*100)} 
+          classes={{
+            root: classes.root,
+          }}
+          />          
+        </Grid>        
+      </Grid>
+      
+    </Box>
+
+    
+  );
+}
--- components/hotel/dashboard.js
+++ components/hotel/dashboard.js
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import Chat0 from './chart0'
 import Chat1 from './chart1'
 import Chat2 from './chart2'
+import Chat3 from './chart3'
 
 export default function Dashboard({dataQuizz}) {
   return (
@@ -12,11 +13,14 @@ export default function Dashboard({dataQuizz}) {
         <Grid item xs={12}>
           <Chat0 dataQuizz={dataQuizz}/>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12}>
           <Chat1 dataQuizz={dataQuizz}/>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12}>
           <Chat2 dataQuizz={dataQuizz}/>
+        </Grid>
+        <Grid item xs={12}>
+          <Chat3 dataQuizz={dataQuizz}/>
         </Grid>        
       </Grid>
       
