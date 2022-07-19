77c0439196ca1ef0b7e3839b9a92c74b4efef1c5
laurods <itrafor@gmail.com>
2022-07-19 12:05:34 -0300

    hotel 190722_22

---
+++ components/hotel/chart0.js
@@ -0,0 +1,54 @@
+import React, {  useState, useEffect, useContext } from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { makeStyles, } from '@material-ui/core/styles';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+
+export default function Chat1({dataQuizz}) {             
+    const totalNo = dataQuizz.reduce((sum, item) => sum + item.answerNoCount, 0);
+    const totalYes = dataQuizz.reduce((sum, item) => sum + item.answerYesCount, 0);
+    const useStyles = makeStyles(theme => ({
+        root: {
+          height: theme.spacing(4),
+          backgroundColor: theme.palette.background.default,
+        },
+        box: {
+            backgroundColor: theme.palette.background.default,
+          },
+      }));
+   
+    
+  return (
+    <Box className={classes.box}>      
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="subtitle1" component="div">          
+            Geral
+          </Typography>                    
+          <span>Avaliações Positivas {Math.round(((totalYes)/(totalYes + totalNo))*100)} %</span>          
+          <LinearProgress 
+          variant="determinate"
+          color ="primary"
+          value={Math.round(((totalYes)/(totalYes + totalNo))*100)} 
+          classes={{
+            root: classes.root,
+          }}          
+          />
+          <span>Avaliações Negativas {Math.round(((totalNo)/(totalYes + totalNo))*100)} %</span>
+          <LinearProgress 
+          variant="determinate"
+          color="secondary"
+          value={Math.round(((totalNo)/(totalYes + totalNo))*100)} 
+          classes={{
+            root: classes.root,
+          }}
+          />          
+        </Grid>        
+      </Grid>
+      
+    </Box>
+
+    
+  );
+}
--- components/hotel/dashboard.js
+++ components/hotel/dashboard.js
@@ -1,12 +1,16 @@
 import React, {  useState, useEffect, useContext } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Chat0 from './chart0'
 import Chat1 from './chart1'
 
 export default function Dashboard({dataQuizz}) {
   return (
     <Box>      
       <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Chat0 dataQuizz={dataQuizz}/>
+        </Grid>
         <Grid item xs={6}>
           <Chat1 dataQuizz={dataQuizz}/>
         </Grid>        
