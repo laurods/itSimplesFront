634f3b1ab7c17feb103a367a87af5247e358f04b
laurods <itrafor@gmail.com>
2022-07-19 18:19:14 -0300

    hotel 190722_53

--- components/hotel/chart.js
+++ components/hotel/chart.js
@@ -4,13 +4,9 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import questions from '../../data/questions' /* lista de perguntas e respostas*/
 
-export default function Chat1({dataQuizz}) {
-    const { data } = questions
-    const { questions } = data
-    console.log('data')
-    console.log(data)
-    console.log('questions')
-    console.log(questions)
+export default function Chat1({dataQuizz}) {   
+    console.log('questions.data.questions')
+    console.log(questions.data.questions)
     
     const feedback = [];
     const listFeedBack = [];
