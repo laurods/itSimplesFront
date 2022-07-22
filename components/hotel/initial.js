467524bc8246c432ffc4f862897876124c65fc3a
laurods <itrafor@gmail.com>
2022-07-22 08:35:56 -0300

    hotel 220722_2

--- components/hotel/initial.js
+++ components/hotel/initial.js
@@ -4,21 +4,28 @@ import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Top from './top';
-import ImageApto from './imageApto'
+import ImageApto from './imageApto';
+import Button from '@mui/material/Button';
 const theme = createTheme();
 
-export default function Dashboard({dataQuizz}) {
+export default function Initial({activeCNPJ}) {
   return (
     <ThemeProvider theme={theme}>      
       <Container>
         <Top/> 
         <Box sx={{ flexGrow: 1, mt: 2 }}>      
           <Grid spacing={2}>
-            <Grid item xs={12}>
-              n. reserva
+            <Grid item xs={6}>
+              n. reserva - cnpj {activeCNPJ}
             </Grid>
-            <Grid item xs={12}>
-              button
+            <Grid item xs={6}>
+                <Button 
+                    variant="outlined" 
+                    size="large"                    
+                    fullWidth
+                    >
+                        Continuar
+                </Button>
             </Grid>                          
           </Grid>         
         </Box>
