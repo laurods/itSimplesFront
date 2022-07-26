80fece84b05a0eb04bdf63797db4b2258f6b79c6
laurods <itrafor@gmail.com>
2022-07-26 18:22:39 -0300

    hotel 260722_33

--- components/hotel/chart1.js
+++ components/hotel/chart1.js
@@ -2,6 +2,7 @@ import React, {  useState, useEffect, useContext } from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import { makeStyles, } from '@material-ui/core/styles';
 
 export default function Chat1({dataQuizz}) {
     const qRuim = [];
@@ -27,6 +28,14 @@ export default function Chat1({dataQuizz}) {
         }        
       })      
       });
+
+      const useStyles = makeStyles(theme => ({
+        root: {      
+          backgroundColor: theme.palette.background.default,
+        },
+      }));
+    
+      const classes = useStyles(); 
    
     
   return (
@@ -42,22 +51,22 @@ export default function Chat1({dataQuizz}) {
           Avaliações: {(qRuim.length + qRegular.length + qBom.length + qOtimo.length)} 
           </Typography>
         </Grid>
-        {qRuim.length > 0 && <Grid item xs={3}>
+        {qRuim.length > 0 && <Grid item xs={6} className={classes.root}>
           <Typography variant="subtitle2" component="div">          
           Ruim: {qRuim.length} | {Math.round(((qRuim.length)/(qRuim.length + qRegular.length + qBom.length + qOtimo.length))*100)}%
           </Typography>
         </Grid>}
-        {qRegular.length> 0 && <Grid item xs={3}>
+        {qRegular.length> 0 && <Grid item xs={6}>
           <Typography variant="subtitle2" component="div">          
           Regular: {qRegular.length} | {Math.round(((qRegular.length)/(qRuim.length + qRegular.length + qBom.length + qOtimo.length))*100)}%
           </Typography>
         </Grid>}
-        {qBom.length > 0 && <Grid item xs={3}>
+        {qBom.length > 0 && <Grid item xs={6}>
           <Typography variant="subtitle2" component="div">          
           Boa: {qBom.length} | {Math.round(((qBom.length)/(qRuim.length + qRegular.length + qBom.length + qOtimo.length))*100)}%
           </Typography>
         </Grid>}
-        {qOtimo.length > 0 && <Grid item xs={3}>
+        {qOtimo.length > 0 && <Grid item xs={6} className={classes.root}>
           <Typography variant="subtitle2" component="div">          
           Ótima: {qOtimo.length} | {Math.round(((qOtimo.length)/(qRuim.length + qRegular.length + qBom.length + qOtimo.length))*100)}%
           </Typography>
