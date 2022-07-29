f1b3c5111eca205d9a85efbbf27d4d4560beed43
laurods <itrafor@gmail.com>
2022-07-29 11:25:53 -0300

    hotel 290722_23

---
+++ pages/api/tenant/getTenantByUserId.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = process.env.MONGODB_URI
+const client = new MongoClient(url);
+ 
+ // The database to use
+ const dbName = process.env.MONGODB_DB;
+                      
+ module.exports = async (req, res) => {
+    try {
+        const { user } = req.body;
+         await client.connect();         
+         const db = client.db(dbName);
+         const col = db.collection("tenant");
+         const tenants = await col.find(
+            { users: [user] },
+            {projection: { _id: 0, name: 1, cnpj: 1 }}
+            ).toArray();     
+         
+         res.status(200).json(tenants);
+
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
+        await client.close();
+    }
+}
--- pages/hotel/adm.js
+++ pages/hotel/adm.js
@@ -14,8 +14,12 @@ export default function Index() {
   useEffect(() => {
     const loadAll = async() =>{
       const cookies = parseCookies()
-      const clients = await axios.post('https://it-simples-front.vercel.app/api/cnpjbyuser', { user: cookies.idUser });              
+      const clients = await axios.post('https://it-simples-front.vercel.app/api/cnpjbyuser', { user: cookies.idUser });
+      const tenants = await axios.post('https://it-simples-front.vercel.app/api/tenant/getTenantByUserId', { user: cookies.idUser });              
       const listClients = clients.data;
+      const listTenants = tenants.data;
+      console.log('listTenants')
+      console.log(listTenants)
       setCNPJsByUsers(listClients)
       setActiveCNPJ(listClients[0].cnpj)     
       
