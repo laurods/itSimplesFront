9a2c9a6f71e9dce5a8c672350386eb824575fec4
laurods <itrafor@gmail.com>
2022-08-03 16:11:16 -0300

    hotel 030822_30

--- components/hotel/adm/viewReservas.js
+++ components/hotel/adm/viewReservas.js
@@ -7,6 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 import EditIcon from '@material-ui/icons/Edit';
 import SendIcon from '@material-ui/icons/Send';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
