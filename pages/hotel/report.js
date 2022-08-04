8e84c97c445769aebae4634430aaf66e29f37f3a
laurods <itrafor@gmail.com>
2022-08-04 11:14:42 -0300

    hotel 040822_15

--- pages/hotel/report.js
+++ pages/hotel/report.js
@@ -6,7 +6,7 @@ import Login from '../../components/login/login';
 import axios from 'axios';
 
 
-export default function Index() { 
+export default function Report() { 
   const {isAuthenticated, setCNPJsByUsers, setActiveCNPJ, setTenantName} = useContext(AuthContext);
   const router = useRouter()
   const { cnpj } = router.query
@@ -14,12 +14,11 @@ export default function Index() {
   console.log(cnpj)
   const [dataFeedback, setDataFeedback] = useState([]);
   const [dataSuggest, setDataSuggest] = useState([]);
-  
+  const [id, setId] = useState('');
 
-  useEffect(() => {
-    const loadAll = async() =>{
-      const FeedBack = [];
-      const quizzes = await axios.post('https://it-simples-front.vercel.app/api/hotel/getQuizzesByCNPJ', { cnpj: cnpj });
+  const FeedBack = [];
+    if(id.length > 0){
+      const quizzes = await axios.post('https://it-simples-front.vercel.app/api/hotel/getQuizzesByCNPJ', { cnpj: id });
       const listQuiz = quizzes.data.quizzes;
       const listSuggest = quizzes.data.suggests;
       listQuiz.map((item)=>{
@@ -34,12 +33,35 @@ export default function Index() {
       setDataFeedback(FeedBack)
       setCNPJsByUsers(listTenants)
       setActiveCNPJ(listTenants[0].cnpj)
-      setTenantName(listTenants[0].name) 
+      setTenantName(listTenants[0].name)
+    }
+       
+  
+
+  // useEffect(() => {
+  //   const loadAll = async() =>{
+  //     const FeedBack = [];
+  //     const quizzes = await axios.post('https://it-simples-front.vercel.app/api/hotel/getQuizzesByCNPJ', { cnpj: cnpj });
+  //     const listQuiz = quizzes.data.quizzes;
+  //     const listSuggest = quizzes.data.suggests;
+  //     listQuiz.map((item)=>{
+  //       item.map(({question, answer})=>{
+  //         FeedBack.push({
+  //             question: question,
+  //             answer:answer            
+  //         })      
+  //       })        
+  //     })
+  //     setDataSuggest(listSuggest)
+  //     setDataFeedback(FeedBack)
+  //     setCNPJsByUsers(listTenants)
+  //     setActiveCNPJ(listTenants[0].cnpj)
+  //     setTenantName(listTenants[0].name) 
           
       
-    }
-    loadAll();
-  }, []);
+  //   }
+  //   loadAll();
+  // }, []);
   
     return (
       <>     
