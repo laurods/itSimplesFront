0131df843938e05f3a5c1b9b4c72d5fd48b2b6fa
laurods <itrafor@gmail.com>
2022-09-01 14:48:03 -0300

    entregas 01092022_25

--- components/dashboard/venda/entregas.js
+++ components/dashboard/venda/entregas.js
@@ -97,7 +97,6 @@ export default function Entregas() {
     const handleFilterViewAll = (phone) => {
         const filteredCustumer = consumers.filter((item) => item.telefone.includes(phone) )
         const dataConsumers = filteredCustumer.slice(0, 10);
-        //setListConsumers(filteredCustumer)
         setListConsumers(dataConsumers)
       
 
